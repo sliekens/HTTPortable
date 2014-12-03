0fa19ae71ec2732814d177fcf53be190bb35a728
Steven Liekens <steven.liekens@gmail.com>
2014-12-03 11:14:08 +0100

    Adding missing file 'IUserAgent.cs'

--- src/Http/IUserAgent.cs
+++ src/Http/IUserAgent.cs
@@ -1,6 +1,12 @@
-﻿namespace Http
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Http
 {
     public interface IUserAgent
     {
+        Task SendAsync(IRequestMessage message, CancellationToken cancellationToken, Func<Stream, CancellationToken, Task> writeAsync = null);
     }
 }
