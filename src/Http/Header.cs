1fbf5cc5d52750392f4749c947279ace8f04c1c9
Steven Liekens <steven.liekens@gmail.com>
2014-12-03 10:41:29 +0100

    Tweaking class 'Header'
    - Setting the initial collection capacity to 1
    - Adding a debugger type proxy class 'HeaderDebugView'

--- src/Http/Header.cs
+++ src/Http/Header.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Http
 {
+    [DebuggerDisplay("Count = {Count}", Name = "{Name}")]
+    [DebuggerTypeProxy(typeof(HeaderDebugView))]
     public class Header : List<string>, IHeader
     {
         public Header(string name)
+            : base(1)
         {
             this.Name = name;
         }
 
         public string Name { get; set; }
+
+        [DebuggerDisplay("{Values}")]
+        private class HeaderDebugView
+        {
+            private readonly Header header;
+
+            public HeaderDebugView(Header header)
+            {
+                this.header = header;
+            }
+
+            [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+            public string[] Values
+            {
+                get
+                {
+                    return header.ToArray();
+                }
+            }
+        }
     }
 }
