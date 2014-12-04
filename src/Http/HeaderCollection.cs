ead40961811a9151dbf21a3330cb246693aa128f
Steven Liekens <steven.liekens@gmail.com>
2014-12-04 15:18:10 +0100

    Setting the equality comparer for 'HeaderCollection'

--- src/Http/HeaderCollection.cs
+++ src/Http/HeaderCollection.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 
 namespace Http
 {
     public abstract class HeaderCollection : KeyedCollection<string, IHeader>, IHeaderCollection
     {
+        public HeaderCollection()
+            : base(StringComparer.Ordinal)
+        {
+        }
+
         protected override string GetKeyForItem(IHeader item)
         {
             return item.Name;
