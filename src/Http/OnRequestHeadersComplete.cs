37da3290cc25a7d2b3d76213ebe52e0d3ad55678
Steven Liekens <steven.liekens@gmail.com>
2014-12-22 10:39:57 +0100

    Adding xml doc comments

--- src/Http/OnRequestHeadersComplete.cs
+++ src/Http/OnRequestHeadersComplete.cs
@@ -4,6 +4,14 @@
     using System.Threading;
     using System.Threading.Tasks;
 
+    /// <summary>Represents the method that is called after a request's headers have been written.</summary>
+    /// <param name="requestMessage">The object that describes the request and its headers.</param>
+    /// <param name="requestStream">The <see cref="T:System.IO.Stream" /> to write a message body to.</param>
+    /// <param name="cancellationToken">
+    /// The <see cref="T:System.Threading.CancellationToken" /> that provides cancellation
+    /// support.
+    /// </param>
+    /// <returns>A <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous write operation.</returns>
     public delegate Task OnRequestHeadersComplete(
         IRequestMessage requestMessage, Stream requestStream, CancellationToken cancellationToken);
 }
--- src/Http/OnResponseHeadersComplete.cs
+++ src/Http/OnResponseHeadersComplete.cs
@@ -4,6 +4,14 @@
     using System.Threading;
     using System.Threading.Tasks;
 
+    /// <summary>Represents the method that is called after a response's headers have been read.</summary>
+    /// <param name="responseMessage">The object that describes the response and its headers.</param>
+    /// <param name="responseStream">The <see cref="T:System.IO.Stream" /> to read a message body from.</param>
+    /// <param name="cancellationToken">
+    /// The <see cref="T:System.Threading.CancellationToken" /> that provides cancellation
+    /// support.
+    /// </param>
+    /// <returns>A <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous read operation.</returns>
     public delegate Task OnResponseHeadersComplete(
         IResponseMessage responseMessage, Stream responseStream, CancellationToken cancellationToken);
 }
