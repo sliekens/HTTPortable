bdef6d006d2606c00430de636f69bb8e7670faf8
Steven Liekens <steven.liekens@gmail.com>
2014-12-22 17:12:47 +0100

    Adding support for 'status-line' tokens

---
+++ src/Http/Grammars/Rfc7230/StatusLineLexer.cs
@@ -0,0 +1,123 @@
+﻿namespace Http.Grammars.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class StatusLineLexer : Lexer<StatusLineToken>
+    {
+        private readonly ILexer<CrLfToken> crLfLexer;
+        private readonly ILexer<HttpVersionToken> httpVersionLexer;
+        private readonly ILexer<ReasonPhraseToken> reasonPhraseLexer;
+        private readonly ILexer<SpToken> spLexer;
+        private readonly ILexer<StatusCodeToken> statusCodeLexer;
+
+        public StatusLineLexer(ILexer<HttpVersionToken> httpVersionLexer, ILexer<SpToken> spLexer,
+            ILexer<StatusCodeToken> statusCodeLexer, ILexer<ReasonPhraseToken> reasonPhraseLexer,
+            ILexer<CrLfToken> crLfLexer)
+        {
+            Contract.Requires(httpVersionLexer != null);
+            Contract.Requires(spLexer != null);
+            Contract.Requires(statusCodeLexer != null);
+            Contract.Requires(reasonPhraseLexer != null);
+            Contract.Requires(crLfLexer != null);
+            this.httpVersionLexer = httpVersionLexer;
+            this.spLexer = spLexer;
+            this.statusCodeLexer = statusCodeLexer;
+            this.reasonPhraseLexer = reasonPhraseLexer;
+            this.crLfLexer = crLfLexer;
+        }
+
+        public override StatusLineToken Read(ITextScanner scanner)
+        {
+            var context = scanner.GetContext();
+            StatusLineToken token;
+            if (this.TryRead(scanner, out token))
+            {
+                return token;
+            }
+
+            throw new SyntaxErrorException(context, "Expected 'status-line'");
+        }
+
+        public override bool TryRead(ITextScanner scanner, out StatusLineToken token)
+        {
+            if (scanner.EndOfInput)
+            {
+                token = default(StatusLineToken);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            HttpVersionToken httpVersion;
+            if (!this.httpVersionLexer.TryRead(scanner, out httpVersion))
+            {
+                token = default(StatusLineToken);
+                return false;
+            }
+
+            SpToken sp1;
+            if (!this.spLexer.TryRead(scanner, out sp1))
+            {
+                this.httpVersionLexer.PutBack(scanner, httpVersion);
+                token = default(StatusLineToken);
+                return false;
+            }
+
+            StatusCodeToken statusCode;
+            if (!this.statusCodeLexer.TryRead(scanner, out statusCode))
+            {
+                this.spLexer.PutBack(scanner, sp1);
+                this.httpVersionLexer.PutBack(scanner, httpVersion);
+                token = default(StatusLineToken);
+                return false;
+            }
+
+            SpToken sp2;
+            if (!this.spLexer.TryRead(scanner, out sp2))
+            {
+                this.statusCodeLexer.PutBack(scanner, statusCode);
+                this.spLexer.PutBack(scanner, sp1);
+                this.httpVersionLexer.PutBack(scanner, httpVersion);
+                token = default(StatusLineToken);
+                return false;
+            }
+
+            ReasonPhraseToken reasonPhrase;
+            if (!this.reasonPhraseLexer.TryRead(scanner, out reasonPhrase))
+            {
+                this.spLexer.PutBack(scanner, sp2);
+                this.statusCodeLexer.PutBack(scanner, statusCode);
+                this.spLexer.PutBack(scanner, sp1);
+                this.httpVersionLexer.PutBack(scanner, httpVersion);
+                token = default(StatusLineToken);
+                return false;
+            }
+
+            CrLfToken crLf;
+            if (!this.crLfLexer.TryRead(scanner, out crLf))
+            {
+                this.reasonPhraseLexer.PutBack(scanner, reasonPhrase);
+                this.spLexer.PutBack(scanner, sp2);
+                this.statusCodeLexer.PutBack(scanner, statusCode);
+                this.spLexer.PutBack(scanner, sp1);
+                this.httpVersionLexer.PutBack(scanner, httpVersion);
+                token = default(StatusLineToken);
+                return false;
+            }
+
+            token = new StatusLineToken(httpVersion, sp1, statusCode, sp2, reasonPhrase, crLf, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.httpVersionLexer != null);
+            Contract.Invariant(this.spLexer != null);
+            Contract.Invariant(this.statusCodeLexer != null);
+            Contract.Invariant(this.reasonPhraseLexer != null);
+            Contract.Invariant(this.crLfLexer != null);
+        }
+    }
+}
---
+++ src/Http/Grammars/Rfc7230/StatusLineToken.cs
@@ -0,0 +1,45 @@
+﻿namespace Http.Grammars.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class StatusLineToken : Token
+    {
+        private readonly CrLfToken crLf;
+        private readonly HttpVersionToken httpVersion;
+        private readonly ReasonPhraseToken reasonPhrase;
+        private readonly SpToken sp1;
+        private readonly SpToken sp2;
+        private readonly StatusCodeToken statusCode;
+
+        public StatusLineToken(HttpVersionToken httpVersion, SpToken sp1, StatusCodeToken statusCode, SpToken sp2,
+            ReasonPhraseToken reasonPhrase, CrLfToken crLf, ITextContext context)
+            : base(string.Concat(httpVersion, sp1, statusCode, sp2, reasonPhrase, crLf), context)
+        {
+            Contract.Requires(httpVersion != null);
+            Contract.Requires(sp1 != null);
+            Contract.Requires(statusCode != null);
+            Contract.Requires(sp2 != null);
+            Contract.Requires(reasonPhrase != null);
+            Contract.Requires(crLf != null);
+            this.httpVersion = httpVersion;
+            this.sp1 = sp1;
+            this.statusCode = statusCode;
+            this.sp2 = sp2;
+            this.reasonPhrase = reasonPhrase;
+            this.crLf = crLf;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.httpVersion != null);
+            Contract.Invariant(this.sp1 != null);
+            Contract.Invariant(this.statusCode != null);
+            Contract.Invariant(this.sp2 != null);
+            Contract.Invariant(this.reasonPhrase != null);
+            Contract.Invariant(this.crLf != null);
+        }
+    }
+}
