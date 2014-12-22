d118f16b3c2210bcc6847865833a77c0d1c03245
Steven Liekens <steven.liekens@gmail.com>
2014-12-22 10:13:57 +0100

    Updating file name

--- src/Http/OnResponseCallback.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Http
-{
-    using System.IO;
-    using System.Threading;
-    using System.Threading.Tasks;
-
-    public delegate Task OnResponseHeadersComplete(IResponseMessage responseMessage, Stream responseStream, CancellationToken cancellationToken);
-}
---
+++ src/Http/OnResponseHeadersComplete.cs
@@ -0,0 +1,9 @@
+﻿namespace Http
+{
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public delegate Task OnResponseHeadersComplete(
+        IResponseMessage responseMessage, Stream responseStream, CancellationToken cancellationToken);
+}
