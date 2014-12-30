bdb63f975099c5775949c9a2ea1d0c0c6f64048f
Steven Liekens <steven.liekens@gmail.com>
2014-12-30 20:14:26 +0100

    Adding support for 'field-vchar' tokens (rfc7230)

---
+++ src/Http/Grammars/Rfc7230/FieldVCharLexer.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using Text.Scanning;
+using Text.Scanning.Core;
+
+namespace Http.Grammars.Rfc7230
+{
+    public class FieldVCharLexer : Lexer<FieldVCharToken>
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly ILexer<ObsTextToken> obsTextLexer;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly ILexer<VCharToken> vCharLexer;
+
+        public FieldVCharLexer()
+            : this(new VCharLexer(), new ObsTextLexer())
+        {
+        }
+
+        public FieldVCharLexer(ILexer<VCharToken> vCharLexer, ILexer<ObsTextToken> obsTextLexer)
+        {
+            Contract.Requires(vCharLexer != null);
+            Contract.Requires(obsTextLexer != null);
+            this.vCharLexer = vCharLexer;
+            this.obsTextLexer = obsTextLexer;
+        }
+
+        public override FieldVCharToken Read(ITextScanner scanner)
+        {
+            FieldVCharToken token;
+            var context = scanner.GetContext();
+            if (TryRead(scanner, out token))
+            {
+                return token;
+            }
+
+            throw new SyntaxErrorException(context, "Expected 'field-vchar'");
+        }
+
+        public override bool TryRead(ITextScanner scanner, out FieldVCharToken token)
+        {
+            var context = scanner.GetContext();
+            VCharToken vCharToken;
+            if (vCharLexer.TryRead(scanner, out vCharToken))
+            {
+                token = new FieldVCharToken(vCharToken, context);
+                return true;
+            }
+
+            ObsTextToken obsTextToken;
+            if (obsTextLexer.TryRead(scanner, out obsTextToken))
+            {
+                token = new FieldVCharToken(obsTextToken, context);
+                return true;
+            }
+
+            token = default(FieldVCharToken);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(vCharLexer != null);
+            Contract.Invariant(obsTextLexer != null);
+        }
+    }
+}
---
+++ src/Http/Grammars/Rfc7230/FieldVCharToken.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using Text.Scanning;
+using Text.Scanning.Core;
+
+namespace Http.Grammars.Rfc7230
+{
+    public class FieldVCharToken : Token
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly ObsTextToken obsTextToken;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly VCharToken vCharToken;
+
+        public FieldVCharToken(VCharToken vCharToken, ITextContext context)
+            : base(vCharToken.Data, context)
+        {
+            Contract.Requires(vCharToken != null);
+            this.vCharToken = vCharToken;
+        }
+
+        public FieldVCharToken(ObsTextToken obsTextToken, ITextContext context)
+            : base(obsTextToken.Data, context)
+        {
+            Contract.Requires(obsTextToken != null);
+            this.obsTextToken = obsTextToken;
+        }
+
+        public ObsTextToken ObsText
+        {
+            get
+            {
+                return obsTextToken;
+            }
+        }
+
+        public VCharToken VChar
+        {
+            get
+            {
+                return vCharToken;
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(vCharToken != null ^ obsTextToken != null);
+        }
+    }
+}
