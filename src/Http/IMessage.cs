eb7b54986e77b78f6e327c74cbb500fe0e2a2a8b
Steven Liekens <steven.liekens@gmail.com>
2015-01-14 13:42:51 +0100

    Adding xml doc comments to 'IMessage'

--- src/Http/IMessage.cs
+++ src/Http/IMessage.cs
@@ -1,7 +1,9 @@
 ﻿namespace Http
 {
+    /// <summary>Provides the interface for types that represent request or response messages.</summary>
     public interface IMessage
     {
+        /// <summary>Gets a collection of message headers.</summary>
         IHeaderCollection Headers { get; }
     }
 }
