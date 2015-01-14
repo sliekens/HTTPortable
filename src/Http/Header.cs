6aaec340aa835a2cebbbe3f8d48a1a4175389546
Steven Liekens <steven.liekens@gmail.com>
2015-01-14 10:53:53 +0100

    Adding xml doc comments to 'Header'

--- src/Http/Header.cs
+++ src/Http/Header.cs
@@ -1,23 +1,31 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-
-namespace Http
+﻿namespace Http
 {
+    using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Diagnostics.Contracts;
 
+    /// <summary>Represents a request or response header.</summary>
     [DebuggerDisplay("Count = {Count}", Name = "{Name}")]
     [DebuggerTypeProxy(typeof(HeaderDebugView))]
     public class Header : List<string>, IHeader
     {
-        private readonly bool required;
         private readonly string name;
+        private readonly bool required;
 
+        /// <summary>Initializes a new instance of the <see cref="T:Http.Header" /> class with a specified header name.</summary>
+        /// <param name="name">The name of the header.</param>
         public Header(string name)
-            : this(name, required: false)
+            : this(name, false)
         {
             Contract.Requires(!string.IsNullOrEmpty(name));
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Http.Header" /> class with a specified header name and an
+        /// additional value that indicates whether the header is required.
+        /// </summary>
+        /// <param name="name">The name of the header.</param>
+        /// <param name="required">A value that indicates whether the header is required.</param>
         public Header(string name, bool required)
             : base(1)
         {
@@ -50,7 +58,6 @@ namespace Http
             Contract.Invariant(!string.IsNullOrEmpty(this.name));
         }
 
-
         [DebuggerDisplay("{Values}")]
         private class HeaderDebugView
         {
@@ -66,7 +73,7 @@ namespace Http
             {
                 get
                 {
-                    return header.ToArray();
+                    return this.header.ToArray();
                 }
             }
         }
