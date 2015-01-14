c64882aa19d3ccf32acf7d1217743213b60a6dd6
Steven Liekens <steven.liekens@gmail.com>
2015-01-14 14:26:47 +0100

    Converting 'ResponseMessage.Headers' to a property with a readonly backing field

--- src/Http/ResponseMessage.cs
+++ src/Http/ResponseMessage.cs
@@ -4,21 +4,36 @@ using System.Threading.Tasks;
 
 namespace Http
 {
+    using System.Diagnostics.Contracts;
+
     public class ResponseMessage : IResponseMessage
     {
         private readonly Task<Stream> getResponseStreamDelegate;
+        private readonly IHeaderCollection headers;
 
         public ResponseMessage()
         {
-            this.Headers = new HeaderCollection();
+            this.headers = new HeaderCollection();
         }
 
-        public IHeaderCollection Headers { get; private set; }
+        public IHeaderCollection Headers
+        {
+            get
+            {
+                return this.headers;
+            }
+        }
 
         public Version Version { get; set; }
 
         public int Status { get; set; }
 
         public string Reason { get; set; }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.headers != null);
+        }
     }
 }
