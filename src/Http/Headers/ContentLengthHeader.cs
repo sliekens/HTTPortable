de8c3fbc1af7cf94b6086e9b6124870495c163b3
Steven Liekens <steven.liekens@gmail.com>
2015-01-15 12:52:19 +0100

    Adding class 'ContentLengthHeader'

---
+++ src/Http/Headers/ContentLengthHeader.cs
@@ -0,0 +1,18 @@
+﻿namespace Http.Headers
+{
+    using System.Globalization;
+
+    public class ContentLengthHeader : Header
+    {
+        public ContentLengthHeader()
+            : base("Content-Length")
+        {
+        }
+
+        public ContentLengthHeader(long contentLength)
+            : base("Content-Length")
+        {
+            this.Add(contentLength.ToString(NumberFormatInfo.InvariantInfo));
+        }
+    }
+}
