b55b01a0102d60225a0f43bfe5257b0f89cfaed6
Steven Liekens <steven.liekens@gmail.com>
2015-02-08 17:13:48 +0100

    Adding support for 'field-name' tokens (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/FieldNameLexer.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics.Contracts;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class FieldNameLexer : Lexer<FieldNameToken>
+    {
+        private readonly ILexer<TokenToken> tokenLexer;
+
+        public FieldNameLexer()
+            : this(new TokenLexer())
+        {
+        }
+
+        public FieldNameLexer(ILexer<TokenToken> tokenLexer)
+        {
+            Contract.Requires(tokenLexer != null);
+            this.tokenLexer = tokenLexer;
+        }
+
+        public override FieldNameToken Read(ITextScanner scanner)
+        {
+            var context = scanner.GetContext();
+            TokenToken tokenToken;
+            try
+            {
+                tokenToken = this.tokenLexer.Read(scanner);
+            }
+            catch (SyntaxErrorException syntaxErrorException)
+            {
+                throw new SyntaxErrorException(context, "Expected 'field-name'", syntaxErrorException);
+
+            }
+
+            return new FieldNameToken(tokenToken, context);
+        }
+
+        public override bool TryRead(ITextScanner scanner, out FieldNameToken token)
+        {
+            if (scanner.EndOfInput)
+            {
+                token = default(FieldNameToken);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            TokenToken tokenToken;
+            if (this.tokenLexer.TryRead(scanner, out tokenToken))
+            {
+                token = new FieldNameToken(tokenToken, context);
+                return true;
+            }
+
+            token = default(FieldNameToken);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.tokenLexer != null);
+        }
+
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/FieldNameToken.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Contracts;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class FieldNameToken : Token
+    {
+        private readonly TokenToken token;
+
+        public FieldNameToken(TokenToken token, ITextContext context)
+            : base(token.Data, context)
+        {
+            Contract.Requires(token != null);
+            this.token = token;
+        }
+
+        public TokenToken Token
+        {
+            get
+            {
+                return this.token;
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.token != null);
+        }
+    }
+}
