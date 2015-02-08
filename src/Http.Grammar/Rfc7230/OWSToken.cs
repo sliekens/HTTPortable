d3cef459656a6b3ee1c64cb7ee8bf75c454815e3
Steven Liekens <steven.liekens@gmail.com>
2015-02-08 22:07:49 +0100

    Restoring a deleted file

---
+++ src/Http.Grammar/Rfc7230/OWSToken.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class OWSToken : Token
+    {
+        public OWSToken(IList<WspMutex> data, ITextContext context)
+            : base(string.Concat(data.Select(mutex => mutex.Token.Data)), context)
+        {
+            Contract.Requires(data != null);
+        }
+    }
+}
