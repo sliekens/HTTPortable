fc70d54285bfd710d55c161f60d2c237bf7bb546
Steven Liekens <steven.liekens@gmail.com>
2015-02-08 23:20:48 +0100

    Adding support for 'field-value' tokens (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/FieldValueToken.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class FieldValueToken : Token
+    {
+        public FieldValueToken(IList<TokenMutex<FieldContentToken, ObsFoldToken>> data, ITextContext context)
+            : base(string.Concat(data.Select(mutex => mutex.Token.Data)), context)
+        {
+        }
+    }
+}
---
+++ tests/Http.Grammar.Tests/Rfc7230/FieldValueLexerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    [TestClass]
+    public class FieldValueLexerTests
+    {
+        [TestMethod]
+        public void ReadFieldValue()
+        {
+            const string input = "application/x-javascript; charset=utf-8";
+            var lexer = new FieldValueLexer();
+            using (ITextScanner scanner = new TextScanner(new StringReader(input)))
+            {
+                scanner.Read();
+                var token = lexer.Read(scanner);
+                Assert.IsNotNull(token);
+                Assert.IsNotNull(token.Data);
+                Assert.AreEqual(input, token.Data);
+            }
+        }
+    }
+}
