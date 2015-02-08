93eb3ebaa6292b574f36afeea05c9d74ad0974c6
Steven Liekens <steven.liekens@gmail.com>
2015-02-08 16:57:40 +0100

    Adding support for 'token' tokens (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/TokenLexer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class TokenLexer : Lexer<TokenToken>
+    {
+        private readonly ILexer<tchar> tcharLexer;
+
+        public TokenLexer()
+            : this(new tcharLexer())
+        {
+        }
+
+        public TokenLexer(ILexer<tchar> tcharLexer)
+        {
+            Contract.Requires(tcharLexer != null);
+            this.tcharLexer = tcharLexer;
+        }
+
+        public override TokenToken Read(ITextScanner scanner)
+        {
+            var context = scanner.GetContext();
+            TokenToken token;
+            if (this.TryRead(scanner, out token))
+            {
+                return token;
+            }
+
+            throw new SyntaxErrorException(context, "Expected 'token'");
+        }
+
+        public override bool TryRead(ITextScanner scanner, out TokenToken token)
+        {
+            if (scanner.EndOfInput)
+            {
+                token = default(TokenToken);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            tchar tchar;
+            if (!this.tcharLexer.TryRead(scanner, out tchar))
+            {
+                token = default(TokenToken);
+                return false;
+            }
+
+            List<tchar> list = new List<tchar> { tchar };
+            while (this.tcharLexer.TryRead(scanner, out tchar))
+            {
+                list.Add(tchar);
+            }
+
+            token = new TokenToken(new ReadOnlyCollection<tchar>(list), context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.tcharLexer != null);
+        }
+
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/TokenToken.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class TokenToken : Token
+    {
+        private readonly IList<tchar> tcharList;
+
+        public TokenToken(IList<tchar> tcharList, ITextContext context)
+            : base(string.Concat(tcharList.Select(tchar => tchar.Data)), context)
+        {
+            Contract.Requires(tcharList != null);
+            Contract.Requires(tcharList.Count > 0);
+            Contract.Requires(Contract.ForAll(tcharList, tchar => tchar != null));
+            this.tcharList = new ReadOnlyCollection<tchar>(tcharList.ToList());
+        }
+
+        public IList<tchar> TCharList
+        {
+            get
+            {
+                return this.tcharList;
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.tcharList != null);
+        }
+
+    }
+}
