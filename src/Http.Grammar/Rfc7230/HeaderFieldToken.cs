698a424922646f3346d04b8e867d4c31a56c9336
Steven Liekens <steven.liekens@gmail.com>
2015-02-08 23:39:54 +0100

    Fixing errors in 'HeaderFieldToken'

--- src/Http.Grammar/Rfc7230/HeaderFieldToken.cs
+++ src/Http.Grammar/Rfc7230/HeaderFieldToken.cs
@@ -1,12 +1,22 @@
-﻿using Text.Scanning;
+﻿using System.Diagnostics.Contracts;
+using Text.Scanning;
 
 namespace Http.Grammar.Rfc7230
 {
     public class HeaderFieldToken : Token
     {
+        private readonly FieldNameToken fieldName;
+        private readonly FieldValueToken fieldValue;
+
         public HeaderFieldToken(FieldNameToken fieldName, OWSToken ows1, FieldValueToken fieldValue, OWSToken ows2, ITextContext context)
-            : base(string.Concat(fieldName.Data, ows1.Data, fieldValue.Data, ows2.Data), context)
+            : base(string.Concat(fieldName.Data, ":", ows1.Data, fieldValue.Data, ows2.Data), context)
         {
+            Contract.Requires(fieldName != null);
+            Contract.Requires(ows1 != null);
+            Contract.Requires(fieldValue != null);
+            Contract.Requires(ows2 != null);
+            this.fieldName = fieldName;
+            this.fieldValue = fieldValue;
         }
     }
 }
