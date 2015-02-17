c509dc1e8cf93d7a85de4d712aca4c29dab6fb2d
Steven Liekens <steven.liekens@gmail.com>
2015-02-17 21:44:31 +0100

    Adding support for 'method' tokens (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/MethodLexer.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.Contracts;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class MethodLexer : Lexer<MethodToken>
+    {
+        private readonly ILexer<TokenToken> tokenLexer;
+
+        public MethodLexer()
+            : this(new TokenLexer())
+        {
+        }
+
+        public MethodLexer(ILexer<TokenToken> tokenLexer)
+        {
+            Contract.Requires(tokenLexer != null);
+            this.tokenLexer = tokenLexer;
+        }
+
+        public override MethodToken Read(ITextScanner scanner)
+        {
+            var context = scanner.GetContext();
+            MethodToken token;
+            if (this.TryRead(scanner, out token))
+            {
+                return token;
+            }
+
+            throw new SyntaxErrorException(context, "Expected 'method'");
+        }
+
+        public override bool TryRead(ITextScanner scanner, out MethodToken token)
+        {
+            var context = scanner.GetContext();
+            TokenToken tokenToken;
+            if (this.tokenLexer.TryRead(scanner, out tokenToken))
+            {
+                token = new MethodToken(tokenToken, context);
+                return true;
+            }
+
+            token = default(MethodToken);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.tokenLexer != null);
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/MethodToken.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Contracts;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    public class MethodToken : Token
+    {
+        private readonly TokenToken token;
+
+        public MethodToken(TokenToken token, ITextContext context)
+            : base(token.Data, context)
+        {
+            Contract.Requires(token != null);
+            this.token = token;
+        }
+
+        public TokenToken Token
+        {
+            get { return token; }
+        }
+    }
+}
