5a49fb1f87f6cf416cd225c99363e98f954bafb2
Steven Liekens <steven.liekens@gmail.com>
2015-02-20 23:09:41 +0100

    Adding a test class for 'RequestLineLexer'

---
+++ tests/Http.Grammar.Tests/Rfc7230/RequestLineLexerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Text.Scanning;
+
+namespace Http.Grammar.Rfc7230
+{
+    [TestClass]
+    public class RequestLineLexerTests
+    {
+        [TestMethod]
+        public void ReadRequestLine1()
+        {
+            const string input = "GET /StevenLiekens/http-client HTTP/1.1";
+            var lexer = new RequestLineLexer();
+            using (TextReader textReader = new StringReader(input))
+            using (ITextScanner textScanner = new TextScanner(textReader))
+            {
+                textScanner.Read();
+                var token = lexer.Read(textScanner);
+                Assert.IsNotNull(token);
+                Assert.IsNotNull(token.Data);
+                Assert.AreEqual(input, token.Data);
+            }
+        }
+    }
+}
