772e40e73da62f143bde863a933d2e6f90853e27
Steven Liekens <steven.liekens@gmail.com>
2015-02-20 23:29:33 +0100

    Adding support for 'gen-delims' tokens (RFC 3986)

---
+++ src/Uri.Grammar/GenDelimsLexer.cs
@@ -0,0 +1,39 @@
+﻿using Text.Scanning;
+
+namespace Uri.Grammar
+{
+    public class GenDelimsLexer : Lexer<GenDelimsToken>
+    {
+        public override GenDelimsToken Read(ITextScanner scanner)
+        {
+            var context = scanner.GetContext();
+            GenDelimsToken token;
+            if (TryRead(scanner, out token))
+            {
+                return token;
+            }
+
+            throw new SyntaxErrorException(context, "Expected 'gen-delims'");
+        }
+
+        public override bool TryRead(ITextScanner scanner, out GenDelimsToken token)
+        {
+            if (scanner.EndOfInput)
+            {
+                return Default(out token);
+            }
+
+            var context = scanner.GetContext();
+            foreach (var c in new[] {':', '/', '?', '#', '[', ']', '@'})
+            {
+                if (scanner.TryMatch(c))
+                {
+                    token = new GenDelimsToken(c, context);
+                    return true;
+                }
+            }
+
+            return Default(out token);
+        }
+    }
+}
---
+++ src/Uri.Grammar/GenDelimsToken.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Contracts;
+using Text.Scanning;
+
+namespace Uri.Grammar
+{
+    public class GenDelimsToken : Token
+    {
+        public GenDelimsToken(char data, ITextContext context)
+            : base(data, context)
+        {
+            Contract.Requires(data == ':'
+                              || data == '/'
+                              || data == '?'
+                              || data == '#'
+                              || data == '['
+                              || data == ']'
+                              || data == '@');
+        }
+    }
+}
