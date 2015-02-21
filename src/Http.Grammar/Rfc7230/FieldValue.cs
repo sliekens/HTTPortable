73a57d93e61242303c1953dd3e013f899e5750bc
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 19:33:30 +0100

    Adding contracts to 'FieldValue'

--- src/Http.Grammar/Rfc7230/FieldValue.cs
+++ src/Http.Grammar/Rfc7230/FieldValue.cs
@@ -4,11 +4,14 @@ using Text.Scanning;
 
 namespace Http.Grammar.Rfc7230
 {
+    using System.Diagnostics.Contracts;
+
     public class FieldValue : Element
     {
         public FieldValue(IList<Alternative<FieldContent, ObsoletedFold>> data, ITextContext context)
             : base(string.Concat(data.Select(element => element.Data)), context)
         {
+            Contract.Requires(data != null);
         }
     }
 }
