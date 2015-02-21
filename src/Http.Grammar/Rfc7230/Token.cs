715882832a6c05af74ff46791c759c817c0f3e04
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 18:26:38 +0100

    Removing fragments from 'Token'

--- src/Http.Grammar/Rfc7230/Token.cs
+++ src/Http.Grammar/Rfc7230/Token.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics.Contracts;
 using System.Linq;
 using Text.Scanning;
@@ -8,30 +7,12 @@ namespace Http.Grammar.Rfc7230
 {
     public class Token : Element
     {
-        private readonly IList<TChar> tCharList;
-
         public Token(IList<TChar> tCharList, ITextContext context)
             : base(string.Concat(tCharList.Select(tchar => tchar.Data)), context)
         {
             Contract.Requires(tCharList != null);
             Contract.Requires(tCharList.Count > 0);
             Contract.Requires(Contract.ForAll(tCharList, tchar => tchar != null));
-            this.tCharList = new ReadOnlyCollection<TChar>(tCharList.ToList());
         }
-
-        public IList<TChar> TCharList
-        {
-            get
-            {
-                return this.tCharList;
-            }
-        }
-
-        [ContractInvariantMethod]
-        private void ObjectInvariant()
-        {
-            Contract.Invariant(this.tCharList != null);
-        }
-
     }
 }
