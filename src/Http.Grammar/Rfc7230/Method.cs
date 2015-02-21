ab625cebb4c37900c67693ac98d2edcd0b8921fc
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 18:25:09 +0100

    Removing a fragment from 'Method'

--- src/Http.Grammar/Rfc7230/Method.cs
+++ src/Http.Grammar/Rfc7230/Method.cs
@@ -5,18 +5,10 @@ namespace Http.Grammar.Rfc7230
 {
     public class Method : Element
     {
-        private readonly Token token;
-
         public Method(Token token, ITextContext context)
             : base(token.Data, context)
         {
             Contract.Requires(token != null);
-            this.token = token;
-        }
-
-        public Token Token
-        {
-            get { return this.token; }
         }
     }
 }
