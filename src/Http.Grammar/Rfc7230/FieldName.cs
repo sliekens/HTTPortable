6db1dd0dfe0c60d5ea5e9ef4adb4ee9e3cc5e768
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 18:24:25 +0100

    Removing a fragment from 'FieldName'

--- src/Http.Grammar/Rfc7230/FieldName.cs
+++ src/Http.Grammar/Rfc7230/FieldName.cs
@@ -5,27 +5,10 @@ namespace Http.Grammar.Rfc7230
 {
     public class FieldName : Element
     {
-        private readonly Token element;
-
         public FieldName(Token element, ITextContext context)
             : base(element.Data, context)
         {
             Contract.Requires(element != null);
-            this.element = element;
-        }
-
-        public Token Element
-        {
-            get
-            {
-                return this.element;
-            }
-        }
-
-        [ContractInvariantMethod]
-        private void ObjectInvariant()
-        {
-            Contract.Invariant(this.element != null);
         }
     }
 }
