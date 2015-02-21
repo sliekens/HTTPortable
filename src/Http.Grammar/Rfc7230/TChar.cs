a3a2d9818042ed634c06b0309259a71478bee44d
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 18:31:54 +0100

    Removing fragments from 'TChar'

--- src/Http.Grammar/Rfc7230/TChar.cs
+++ src/Http.Grammar/Rfc7230/TChar.cs
@@ -6,9 +6,6 @@ namespace Http.Grammar.Rfc7230
 {
     public class TChar : Element
     {
-        private readonly Alpha alpha;
-        private readonly Digit digit;
-
         public TChar(char data, ITextContext context)
             : base(data, context)
         {
@@ -21,30 +18,12 @@ namespace Http.Grammar.Rfc7230
             : base(digit.Data, context)
         {
             Contract.Requires(digit != null);
-            this.digit = digit;
         }
 
         public TChar(Alpha alpha, ITextContext context)
             : base(alpha.Data, context)
         {
             Contract.Requires(alpha != null);
-            this.alpha = alpha;
-        }
-
-        public Alpha Alpha
-        {
-            get { return alpha; }
-        }
-
-        public Digit Digit
-        {
-            get { return digit; }
-        }
-
-        [ContractInvariantMethod]
-        private void ObjectInvariant()
-        {
-            Contract.Invariant(this.Alpha == null || this.Digit == null);
         }
     }
 }
