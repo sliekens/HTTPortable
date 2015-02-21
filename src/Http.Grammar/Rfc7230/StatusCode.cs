b87d394ff7123d87675b3ac1d8af0117d8e795a7
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 18:33:54 +0100

    Adding contracts to 'StatusCode'

--- src/Http.Grammar/Rfc7230/StatusCode.cs
+++ src/Http.Grammar/Rfc7230/StatusCode.cs
@@ -3,12 +3,16 @@ using Text.Scanning.Core;
 
 namespace Http.Grammar.Rfc7230
 {
+    using System.Diagnostics.Contracts;
 
     public class StatusCode : Element
     {
         public StatusCode(Digit digit1, Digit digit2, Digit digit3, ITextContext context)
             : base(string.Concat(digit1.Data, digit2.Data, digit3.Data), context)
         {
+            Contract.Requires(digit1 != null);
+            Contract.Requires(digit2 != null);
+            Contract.Requires(digit3 != null);
         }
 
         public int ToInt()
