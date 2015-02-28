6a8ffd7ee3e3c99e20648276610aa2638dd7f917
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 01:24:46 +0100

    Adding support for 'segment-nz' elements (RFC 3986)

---
+++ src/Uri.Grammar/SegmentNonZero.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class SegmentNonZero : Element
+    {
+        public SegmentNonZero(IList<PathCharacter> pathCharacters, ITextContext context)
+            : base(string.Concat(pathCharacters.Select(character => character.Data)), context)
+        {
+            Contract.Requires(pathCharacters != null);
+            Contract.Requires(pathCharacters.Count != 0);
+            Contract.Requires(Contract.ForAll(pathCharacters, character => character != null));
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Uri.Grammar/SegmentNonZeroLexer.cs
@@ -0,0 +1,56 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class SegmentNonZeroLexer : Lexer<SegmentNonZero>
+    {
+        private readonly ILexer<PathCharacter> pathCharacterLexer;
+
+        public SegmentNonZeroLexer()
+            : this(new PathCharacterLexer())
+        {
+        }
+
+        public SegmentNonZeroLexer(ILexer<PathCharacter> pathCharacterLexer)
+            : base("segment-nz")
+        {
+            Contract.Requires(pathCharacterLexer != null);
+            this.pathCharacterLexer = pathCharacterLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out SegmentNonZero element)
+        {
+            var elements = new List<PathCharacter>();
+            var context = scanner.GetContext();
+            do
+            {
+                PathCharacter pathCharacter;
+                if (!this.pathCharacterLexer.TryRead(scanner, out pathCharacter))
+                {
+                    break;
+                }
+
+                elements.Add(pathCharacter);
+            }
+            while (!scanner.EndOfInput);
+
+            if (elements.Count == 0)
+            {
+                element = default(SegmentNonZero);
+                return false;
+            }
+
+            element = new SegmentNonZero(elements, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.pathCharacterLexer != null);
+        }
+    }
+}
