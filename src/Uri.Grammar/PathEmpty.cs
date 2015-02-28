b054be875ee2e3ee78ba36b428f1cf754a544443
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 11:58:19 +0100

    Adding support for 'path-empty' elements (RFC 3986)
    This element does not need a lexer as far as I can tell. It's always an empty string.
    
    The grammar:
    path-empty = 0<pchar>
    
    I can't think of any meaningful way to validate a string like that.

---
+++ src/Uri.Grammar/PathEmpty.cs
@@ -0,0 +1,12 @@
+﻿namespace Uri.Grammar
+{
+    using Text.Scanning;
+
+    public class PathEmpty : Element
+    {
+        public PathEmpty(ITextContext context)
+            : base(string.Empty, context)
+        {
+        }
+    }
+}
