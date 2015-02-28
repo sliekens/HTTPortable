3fe578237a50975db036590e8920b1e074706466
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 13:40:56 +0100

    Adding support for 'path-absolute' elements (RFC 3986)

---
+++ src/Uri.Grammar/PathAbsolute.cs
@@ -0,0 +1,22 @@
+﻿namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class PathAbsolute : Element
+    {
+        public PathAbsolute(ITextContext context)
+            : base("/", context)
+        {
+            Contract.Requires(context != null);
+        }
+
+        public PathAbsolute(PathRootless path, ITextContext context)
+            : base(string.Concat("/", path.Data), context)
+        {
+            Contract.Requires(path != null);
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Uri.Grammar/PathAbsoluteLexer.cs
@@ -0,0 +1,57 @@
+﻿namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class PathAbsoluteLexer : Lexer<PathAbsolute>
+    {
+        private readonly ILexer<PathRootless> pathRootlessLexer;
+
+        public PathAbsoluteLexer()
+            : this(new PathRootlessLexer())
+        {
+        }
+
+        public PathAbsoluteLexer(ILexer<PathRootless> pathRootlessLexer)
+            : base("path-absolute")
+        {
+            Contract.Requires(pathRootlessLexer != null);
+            this.pathRootlessLexer = pathRootlessLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out PathAbsolute element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(PathAbsolute);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            if (!scanner.TryMatch('/'))
+            {
+                element = default(PathAbsolute);
+                return false;
+            }
+
+            PathRootless path;
+            if (this.pathRootlessLexer.TryRead(scanner, out path))
+            {
+                element = new PathAbsolute(path, context);
+            }
+            else
+            {
+                element = new PathAbsolute(context);
+            }
+
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.pathRootlessLexer != null);
+        }
+    }
+}
