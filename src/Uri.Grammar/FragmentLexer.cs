23c7b491f983f565387e2c93fd48066f5c02f3fe
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 01:06:11 +0100

    Adding support for 'fragment' elements (RFC 3986)

---
+++ src/Uri.Grammar/FragmentLexer.cs
@@ -0,0 +1,64 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class FragmentLexer : Lexer<Fragment>
+    {
+        private readonly ILexer<PathCharacter> pathCharacterLexer;
+
+        public FragmentLexer()
+            : this(new PathCharacterLexer())
+        {
+        }
+
+        public FragmentLexer(ILexer<PathCharacter> pathCharacterLexer)
+            : base("fragment")
+        {
+            Contract.Requires(pathCharacterLexer != null);
+            this.pathCharacterLexer = pathCharacterLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out Fragment element)
+        {
+            var elements = new List<Alternative<PathCharacter, Element>>();
+            var context = scanner.GetContext();
+            element = new Fragment(elements, context);
+            do
+            {
+                PathCharacter pathCharacter;
+                if (this.pathCharacterLexer.TryRead(scanner, out pathCharacter))
+                {
+                    elements.Add(new Alternative<PathCharacter, Element>(pathCharacter, context));
+                }
+                else if (scanner.TryMatch('/'))
+                {
+                    var e = new Element('/', context);
+                    elements.Add(new Alternative<PathCharacter, Element>(e, context));
+                }
+                else if (scanner.TryMatch('?'))
+                {
+                    var e = new Element('?', context);
+                    elements.Add(new Alternative<PathCharacter, Element>(e, context));
+                }
+                else
+                {
+                    break;
+                }
+
+                context = scanner.GetContext();
+            }
+            while (!scanner.EndOfInput);
+
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.pathCharacterLexer != null);
+        }
+    }
+}
