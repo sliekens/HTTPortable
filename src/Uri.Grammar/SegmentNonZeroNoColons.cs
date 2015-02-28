87efdf35021e84f77421374588974d4f13717ff5
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 11:52:58 +0100

    Adding support for 'segment-nz-nc' elements (RFC 3986)

---
+++ src/Uri.Grammar/SegmentNonZeroNoColons.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class SegmentNonZeroNoColons : Element
+    {
+        public SegmentNonZeroNoColons(IList<Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter, Element>> pathCharacters, ITextContext context)
+            : base(string.Concat(pathCharacters.Select(character => character.Data)), context)
+        {
+            Contract.Requires(pathCharacters != null);
+            Contract.Requires(pathCharacters.Count != 0);
+            Contract.Requires(
+                Contract.ForAll(pathCharacters,
+                alternative =>
+                {
+                    if (alternative == null)
+                    {
+                        return false;
+                    }
+
+                    var element = alternative.Element;
+                    if (element == null)
+                    {
+                        return false;
+                    }
+
+
+                    if (element is Unreserved)
+                    {
+                        return true;
+                    }
+
+                    if (element is PercentEncoding)
+                    {
+                        return true;
+                    }
+
+                    if (element is SubcomponentsDelimiter)
+                    {
+                        return true;
+                    }
+
+                    if (element.Data == "@")
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }));
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Uri.Grammar/SegmentNonZeroNoColonsLexer.cs
@@ -0,0 +1,95 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class SegmentNonZeroNoColonsLexer : Lexer<SegmentNonZeroNoColons>
+    {
+        private readonly ILexer<Unreserved> unreservedLexer;
+
+        private readonly ILexer<PercentEncoding> percentEncodingLexer;
+
+        private readonly ILexer<SubcomponentsDelimiter> subcomponentsLexer;
+
+        public SegmentNonZeroNoColonsLexer()
+            : this(new UnreservedLexer(), new PercentEncodingLexer(), new SubcomponentsDelimiterLexer())
+        {
+        }
+
+        public SegmentNonZeroNoColonsLexer(ILexer<Unreserved> unreservedLexer, ILexer<PercentEncoding> percentEncodingLexer, ILexer<SubcomponentsDelimiter> subcomponentsLexer)
+            : base("segment-nz-nc")
+        {
+            Contract.Requires(unreservedLexer != null);
+            Contract.Requires(percentEncodingLexer != null);
+            Contract.Requires(subcomponentsLexer != null);
+            this.unreservedLexer = unreservedLexer;
+            this.percentEncodingLexer = percentEncodingLexer;
+            this.subcomponentsLexer = subcomponentsLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out SegmentNonZeroNoColons element)
+        {
+            var elements = new List<Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter, Element>>();
+            var context = scanner.GetContext();
+            while (!scanner.EndOfInput)
+            {
+                var innerContext = scanner.GetContext();
+                Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter, Element> alternative;
+                Unreserved unreserved;
+                if (this.unreservedLexer.TryRead(scanner, out unreserved))
+                {
+                    alternative = new Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter, Element>(unreserved, innerContext);
+                }
+                else
+                {
+                    PercentEncoding percentEncoding;
+                    if (this.percentEncodingLexer.TryRead(scanner, out percentEncoding))
+                    {
+                        alternative = new Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter, Element>(percentEncoding, innerContext);
+                    }
+                    else
+                    {
+                        SubcomponentsDelimiter subcomponentsDelimiter;
+                        if (this.subcomponentsLexer.TryRead(scanner, out subcomponentsDelimiter))
+                        {
+                            alternative = new Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter, Element>(percentEncoding, innerContext);
+                        }
+                        else
+                        {
+                            if (scanner.TryMatch('@'))
+                            {
+                                var terminal = new Element('@', innerContext);
+                                alternative = new Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter, Element>(terminal, innerContext);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                elements.Add(alternative);
+            }
+
+            if (elements.Count == 0)
+            {
+                element = default(SegmentNonZeroNoColons);
+                return false;
+            }
+
+            element = new SegmentNonZeroNoColons(elements, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.unreservedLexer != null);
+            Contract.Invariant(this.percentEncodingLexer != null);
+            Contract.Invariant(this.subcomponentsLexer != null);
+        }
+    }
+}
