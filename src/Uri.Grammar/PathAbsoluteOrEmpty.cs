ad98281b592b698effef1fe183a163c9868b1ed8
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 13:55:50 +0100

    Adding properties 'IsAbsolute' and 'IsEmpty' to 'PathAbsoluteOrEmpty'

--- src/Uri.Grammar/PathAbsoluteOrEmpty.cs
+++ src/Uri.Grammar/PathAbsoluteOrEmpty.cs
@@ -8,12 +8,40 @@
 
     public class PathAbsoluteOrEmpty : Element
     {
+        private readonly bool isEmpty;
+
+        private readonly bool isAbsolute;
+
         public PathAbsoluteOrEmpty(IList<Sequence<Element, Segment>> path, ITextContext context)
             : base(string.Concat(path.Select(sequence => sequence.Data)), context)
         {
             Contract.Requires(path != null);
             Contract.Requires(Contract.ForAll(path, sequence => sequence.Element1.Data == "/"));
             Contract.Requires(context != null);
+            if (path.Count == 0)
+            {
+                this.isEmpty = true;
+            }
+            else
+            {
+                this.isAbsolute = true;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return this.isEmpty;
+            }
+        }
+
+        public bool IsAbsolute
+        {
+            get
+            {
+                return this.isAbsolute;
+            }
         }
     }
 }
