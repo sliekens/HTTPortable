42374b574587453abdb6ec677ecd5732e23af244
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 11:59:04 +0100

    Adding missing requires to 'PathEmpty'

--- src/Uri.Grammar/PathEmpty.cs
+++ src/Uri.Grammar/PathEmpty.cs
@@ -1,5 +1,7 @@
 ﻿namespace Uri.Grammar
 {
+    using System.Diagnostics.Contracts;
+
     using Text.Scanning;
 
     public class PathEmpty : Element
@@ -7,6 +9,7 @@
         public PathEmpty(ITextContext context)
             : base(string.Empty, context)
         {
+            Contract.Requires(context != null);
         }
     }
 }
