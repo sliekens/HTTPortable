f22480efedcf8e137357544aa2fa703583a76ad4
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 14:55:20 +0100

    Adding support for 'reg-name' elements (RFC 3986)

---
+++ src/Uri.Grammar/RegisteredName.cs
@@ -0,0 +1,19 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+
+    using Text.Scanning;
+
+    public class RegisteredName : Element
+    {
+        public RegisteredName(IList<Alternative<Unreserved, PercentEncoding, SubcomponentsDelimiter>> elements, ITextContext context)
+            : base(string.Concat(elements.Select(element => element.Data)), context)
+        {
+            Contract.Requires(elements != null);
+            Contract.Requires(Contract.ForAll(elements, element => element != null));
+            Contract.Requires(context != null);
+        }
+    }
+}
