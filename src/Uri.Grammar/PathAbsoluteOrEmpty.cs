32c123a54bcb9a26976780c7ebacd6f02b918e33
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 13:51:49 +0100

    Adding support for 'path-abempty' elements (RFC 3986)

---
+++ src/Uri.Grammar/PathAbsoluteOrEmpty.cs
@@ -0,0 +1,19 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+
+    using Text.Scanning;
+
+    public class PathAbsoluteOrEmpty : Element
+    {
+        public PathAbsoluteOrEmpty(IList<Sequence<Element, Segment>> path, ITextContext context)
+            : base(string.Concat(path.Select(sequence => sequence.Data)), context)
+        {
+            Contract.Requires(path != null);
+            Contract.Requires(Contract.ForAll(path, sequence => sequence.Element1.Data == "/"));
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Uri.Grammar/PathAbsoluteOrEmptyLexer.cs
@@ -0,0 +1,57 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class PathAbsoluteOrEmptyLexer : Lexer<PathAbsoluteOrEmpty>
+    {
+        private readonly ILexer<Segment> segmentLexer;
+
+        public PathAbsoluteOrEmptyLexer()
+            :this(new SegmentLexer())
+        {
+        }
+
+        public PathAbsoluteOrEmptyLexer(ILexer<Segment> segmentLexer)
+            : base("path-abempty")
+        {
+            Contract.Requires(segmentLexer != null);
+            this.segmentLexer = segmentLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out PathAbsoluteOrEmpty element)
+        {
+            var context = scanner.GetContext();
+            var innerContext = scanner.GetContext();
+            var elements = new List<Sequence<Element, Segment>>();
+            while (scanner.TryMatch('/'))
+            {
+                Segment segment;
+                if (this.segmentLexer.TryRead(scanner, out segment))
+                {
+                    var terminal = new Element('/', innerContext);
+                    var sequence = new Sequence<Element, Segment>(terminal, segment, innerContext);
+                    elements.Add(sequence);
+                }
+                else
+                {
+                    scanner.PutBack('/');
+                    break;
+                }
+
+                innerContext = scanner.GetContext();
+            }
+
+            element = new PathAbsoluteOrEmpty(elements, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.segmentLexer != null);
+        }
+    }
+}
