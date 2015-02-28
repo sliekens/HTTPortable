bd637b08a0c053727708a7614c90dfc53f8f6173
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 17:31:32 +0100

    Fixing bugs in 'QueryLexer'

--- src/Uri.Grammar/QueryLexer.cs
+++ src/Uri.Grammar/QueryLexer.cs
@@ -25,23 +25,23 @@
         {
             var elements = new List<Alternative<PathCharacter, Element>>();
             var context = scanner.GetContext();
-            element = new Query(elements, context);
-            do
+            while (!scanner.EndOfInput)
             {
+                var innerContext = scanner.GetContext();
                 PathCharacter pathCharacter;
                 if (this.pathCharacterLexer.TryRead(scanner, out pathCharacter))
                 {
-                    elements.Add(new Alternative<PathCharacter, Element>(pathCharacter, context));
+                    elements.Add(new Alternative<PathCharacter, Element>(pathCharacter, innerContext));
                 }
-                else if (scanner.TryMatch('/'))
+                else if (!scanner.EndOfInput && scanner.TryMatch('/'))
                 {
-                    var e = new Element('/', context);
-                    elements.Add(new Alternative<PathCharacter, Element>(e, context));
+                    var e = new Element('/', innerContext);
+                    elements.Add(new Alternative<PathCharacter, Element>(e, innerContext));
                 }
-                else if (scanner.TryMatch('?'))
+                else if (!scanner.EndOfInput && scanner.TryMatch('?'))
                 {
-                    var e = new Element('?', context);
-                    elements.Add(new Alternative<PathCharacter, Element>(e, context));
+                    var e = new Element('?', innerContext);
+                    elements.Add(new Alternative<PathCharacter, Element>(e, innerContext));
                 }
                 else
                 {
@@ -50,8 +50,8 @@
 
                 context = scanner.GetContext();
             }
-            while (!scanner.EndOfInput);
 
+            element = new Query(elements, context);
             return true;
         }
 
