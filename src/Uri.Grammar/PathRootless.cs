298fec6d3cfc065da7034f8a6a81d5f60d9a89aa
Steven Liekens <steven.liekens@gmail.com>
2015-02-28 13:15:42 +0100

    Adding support for 'path-rootless' elements (RFC 3986)

---
+++ src/Uri.Grammar/PathRootless.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class PathRootless : Element
+    {
+        public PathRootless(SegmentNonZero startSegment, IList<Sequence<Element, Segment>> segments, ITextContext context)
+            : base(string.Concat(startSegment.Data, string.Concat(segments.Select(sequence => sequence.Data))), context)
+        {
+            Contract.Requires(startSegment != null);
+            Contract.Requires(segments != null);
+            Contract.Requires(Contract.ForAll(segments, sequence => sequence != null));
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Uri.Grammar/PathRootlessLexer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uri.Grammar
+{
+    using Text.Scanning;
+
+    public class PathRootlessLexer : Lexer<PathRootless>
+    {
+        private readonly ILexer<SegmentNonZero> segmentNonZeroLexer;
+
+        private readonly ILexer<Segment> segmentLexer;
+
+        public PathRootlessLexer()
+            : this(new SegmentNonZeroLexer(), new SegmentLexer())
+        {
+        }
+
+        public PathRootlessLexer(ILexer<SegmentNonZero> segmentNonZeroLexer, ILexer<Segment> segmentLexer)
+            : base("path-rootless")
+        {
+            this.segmentNonZeroLexer = segmentNonZeroLexer;
+            this.segmentLexer = segmentLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out PathRootless element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(PathRootless);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            SegmentNonZero segmentNonZero;
+            if (!this.segmentNonZeroLexer.TryRead(scanner, out segmentNonZero))
+            {
+                element = default(PathRootless);
+                return false;
+            }
+
+            var segments = new List<Sequence<Element, Segment>>();
+            var innerContext = scanner.GetContext();
+            while (scanner.TryMatch('/'))
+            {
+                var terminal = new Element('/', innerContext);
+                Segment segment;
+                if (this.segmentLexer.TryRead(scanner, out segment))
+                {
+                    var sequence = new Sequence<Element, Segment>(terminal, segment, innerContext);
+                    segments.Add(sequence);
+                }
+                else
+                {
+                    scanner.PutBack('/');
+                    break;
+                }
+
+                innerContext = scanner.GetContext();
+            }
+
+            element = new PathRootless(segmentNonZero, segments, context);
+            return true;
+        }
+    }
+}
