eeea615cbfe48940b17a1848869659b4662619ca
Steven Liekens <steven.liekens@gmail.com>
2015-03-03 23:22:50 +0100

    Improving support for 'IPv6address' elements (RFC 3986)
    The seventh alternative of the grammar is now supported.

--- src/Uri.Grammar/IPv6AddressLexer.cs
+++ src/Uri.Grammar/IPv6AddressLexer.cs
@@ -612,8 +612,29 @@
 
         private bool TryReadSeventhPass(ITextScanner scanner, out IPv6Address element)
         {
-            element = default(IPv6Address);
-            return false;
+            var context = scanner.GetContext();
+
+            var optionals = this.ReadOptionalInt16s(scanner, 5);
+            if (optionals == null)
+            {
+                element = default(IPv6Address);
+                return false;
+            }
+
+            LeastSignificantInt32 bits32;
+            if (!this.leastSignificantInt32Lexer.TryRead(scanner, out bits32))
+            {
+                for (int i = optionals.Count - 1; i >= 0; i--)
+                {
+                    scanner.PutBack(optionals[i].Data);
+                }
+
+                element = default(IPv6Address);
+                return false;
+            }
+
+            element = new IPv6Address(string.Concat(string.Concat(optionals.Select(o => o.Data)), bits32.Data), context);
+            return true;
         }
 
         private bool TryReadEighthPass(ITextScanner scanner, out IPv6Address element)
