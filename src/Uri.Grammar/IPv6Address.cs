0a0a4a9a50d9391968a8df76d7929f29ec36e093
Steven Liekens <steven.liekens@gmail.com>
2015-03-04 21:03:17 +0100

    Adding TODO comments to 'IPv6Address'

--- src/Uri.Grammar/IPv6Address.cs
+++ src/Uri.Grammar/IPv6Address.cs
@@ -7,6 +7,8 @@
         public IPv6Address(string address, ITextContext context)
             : base(address, context)
         {
+            // TODO: validate that 'address' is actually a valid IPv6 address
+            // TODO: figure out how to validate IPv6 addresses without re-implementing the entire parser
         }
     }
 }
