4e51bcea0f62bf5558c04d3d6880681ae3b873fd
Steven Liekens <steven.liekens@gmail.com>
2015-03-05 22:33:21 +0100

    Adding unit tests for 'URI' elements

---
+++ src/Uri.Grammar.Tests/UriLexerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Uri.Grammar
+{
+    using System.IO;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Text.Scanning;
+
+    [TestClass]
+    public class UriLexerTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "URI.xml", "Row", DataAccessMethod.Sequential)]
+        public void ReadUri()
+        {
+            var dataRow = this.TestContext.DataRow;
+            var input = (string)dataRow["Input"];
+            var expected = (string)dataRow["Expected"];
+            var lexer = new UriLexer();
+            using (TextReader textReader = new StringReader(input))
+            using (ITextScanner scanner = new TextScanner(textReader))
+            {
+                scanner.Read();
+                var element = lexer.Read(scanner);
+                Assert.IsNotNull(element);
+                Assert.AreEqual(expected, element.Data);
+            }
+        }
+    }
+}
