466674468fdcd79f414d4d72431f296bea206cb3
Steven Liekens <steven.liekens@gmail.com>
2015-03-05 19:27:58 +0100

    Adding support for 'host' elements (RFC 3986)

---
+++ src/Uri.Grammar/Host.cs
@@ -0,0 +1,30 @@
+﻿namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class Host : Element
+    {
+        public Host(IPLiteral ipLiteral, ITextContext context)
+            : base(ipLiteral.Data, context)
+        {
+            Contract.Requires(ipLiteral != null);
+            Contract.Requires(context != null);
+        }
+
+        public Host(IPv4Address ipv4Address, ITextContext context)
+            : base(ipv4Address.Data, context)
+        {
+            Contract.Requires(ipv4Address != null);
+            Contract.Requires(context != null);
+        }
+
+        public Host(RegisteredName registeredName, ITextContext context)
+            : base(registeredName.Data, context)
+        {
+            Contract.Requires(registeredName != null);
+            Contract.Requires(context != null);
+        }
+    }
+}
