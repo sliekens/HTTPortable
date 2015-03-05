002c24589a7c1168c6f0ae40ef0f404081c7df32
Steven Liekens <steven.liekens@gmail.com>
2015-03-05 21:45:39 +0100

    Adding support for 'hier-part' elements (RFC 3986)

---
+++ src/Uri.Grammar/HierarchicalPart.cs
@@ -0,0 +1,12 @@
+﻿namespace Uri.Grammar
+{
+    using Text.Scanning;
+
+    public class HierarchicalPart : Element
+    {
+        public HierarchicalPart(string data, ITextContext context)
+            : base(data, context)
+        {
+        }
+    }
+}
