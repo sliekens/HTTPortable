8a503076b84cc45b14102ba20c0cdb1aa39155e3
Steven Liekens <steven.liekens@gmail.com>
2015-03-05 19:17:05 +0100

    Adding support for 'port' elements (RFC 3986)

---
+++ src/Uri.Grammar/Port.cs
@@ -0,0 +1,18 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class Port : Element
+    {
+        public Port(IList<Digit> digits, ITextContext context)
+            : base(string.Concat(digits), context)
+        {
+            Contract.Requires(digits != null);
+            Contract.Requires(Contract.ForAll(digits, digit => digit != null));
+        }
+    }
+}
---
+++ src/Uri.Grammar/PortLexer.cs
@@ -0,0 +1,45 @@
+﻿namespace Uri.Grammar
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class PortLexer : Lexer<Port>
+    {
+        private readonly ILexer<Digit> digitLexer;
+
+        public PortLexer()
+            : this(new DigitLexer())
+        {
+        }
+
+        public PortLexer(ILexer<Digit> digitLexer)
+            : base("port")
+        {
+            Contract.Requires(digitLexer != null);
+            this.digitLexer = digitLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out Port element)
+        {
+            var context = scanner.GetContext();
+            var digits = new List<Digit>();
+            Digit digit;
+            while (this.digitLexer.TryRead(scanner, out digit))
+            {
+                digits.Add(digit);
+            }
+
+            element = new Port(digits, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.digitLexer != null);
+        }
+    }
+}
