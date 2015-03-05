d1a348c95ad2b06ebceac2d76977d08bec376704
Steven Liekens <steven.liekens@gmail.com>
2015-03-05 21:37:02 +0100

    Adding support for 'relative-ref' elements (RFC 3986)

---
+++ src/Uri.Grammar/RelativeReferenceLexer.cs
@@ -0,0 +1,162 @@
+﻿namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using QueryPart = Text.Scanning.Sequence<Text.Scanning.Element, Query>;
+    using FragmentPart = Text.Scanning.Sequence<Text.Scanning.Element, Fragment>;
+
+    public class RelativeReferenceLexer : Lexer<RelativeReference>
+    {
+        private readonly ILexer<RelativePart> relativePartLexer;
+
+        private readonly ILexer<Query> queryLexer;
+
+        private readonly ILexer<Fragment> fragmentLexer;
+
+        public RelativeReferenceLexer()
+            : this(new RelativePartLexer(), new QueryLexer(), new FragmentLexer())
+        {
+        }
+
+        public RelativeReferenceLexer(ILexer<RelativePart> relativePartLexer, ILexer<Query> queryLexer, ILexer<Fragment> fragmentLexer)
+            : base("relative-ref")
+        {
+            Contract.Requires(relativePartLexer != null);
+            Contract.Requires(queryLexer != null);
+            Contract.Requires(fragmentLexer != null);
+            this.relativePartLexer = relativePartLexer;
+            this.queryLexer = queryLexer;
+            this.fragmentLexer = fragmentLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out RelativeReference element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(RelativeReference);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            RelativePart relativePart;
+            if (!this.relativePartLexer.TryRead(scanner, out relativePart))
+            {
+                element = default(RelativeReference);
+                return false;
+            }
+
+            QueryPart queryPart;
+            var hasQueryPart = this.TryReadQueryPart(scanner, out queryPart);
+
+            FragmentPart fragmentPart;
+            var hasFragmentPart = this.TryReadFragmentPart(scanner, out fragmentPart);
+
+            element = new RelativeReference(string.Concat(relativePart, queryPart, fragmentPart), context);
+            return true;
+        }
+
+
+        private bool TryReadQueryPart(ITextScanner scanner, out QueryPart element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(QueryPart);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Element questionMark;
+            if (!this.TryReadQuestionMark(scanner, out questionMark))
+            {
+                element = default(QueryPart);
+                return false;
+            }
+
+            Query query;
+            if (!this.queryLexer.TryRead(scanner, out query))
+            {
+                scanner.PutBack(questionMark.Data);
+                element = default(QueryPart);
+                return false;
+            }
+
+            element = new QueryPart(questionMark, query, context);
+            return true;
+        }
+
+        private bool TryReadFragmentPart(ITextScanner scanner, out FragmentPart element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(FragmentPart);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Element numberSign;
+            if (!this.TryReadNumberSign(scanner, out numberSign))
+            {
+                element = default(FragmentPart);
+                return false;
+            }
+
+            Fragment fragment;
+            if (!this.fragmentLexer.TryRead(scanner, out fragment))
+            {
+                scanner.PutBack(numberSign.Data);
+                element = default(FragmentPart);
+                return false;
+            }
+
+            element = new FragmentPart(numberSign, fragment, context);
+            return true;
+        }
+
+        private bool TryReadQuestionMark(ITextScanner scanner, out Element element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(Element);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            if (scanner.TryMatch('?'))
+            {
+                element = new Element("?", context);
+                return true;
+            }
+
+            element = default(Element);
+            return false;
+        }
+
+        private bool TryReadNumberSign(ITextScanner scanner, out Element element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(Element);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            if (scanner.TryMatch('#'))
+            {
+                element = new Element("#", context);
+                return true;
+            }
+
+            element = default(Element);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.relativePartLexer != null);
+            Contract.Invariant(this.queryLexer != null);
+            Contract.Invariant(this.fragmentLexer != null);
+        }
+    }
+}
