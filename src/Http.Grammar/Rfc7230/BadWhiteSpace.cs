72d4e8f1ec39b8b7a8b18a47dbd4ec8ab2fd4b56
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 16:25:03 +0100

    Adding support for 'BWS' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/BadWhiteSpace.cs
@@ -0,0 +1,16 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class BadWhiteSpace : Element
+    {
+        public BadWhiteSpace(OptionalWhiteSpace optionalWhiteSpace, ITextContext context)
+            : base(optionalWhiteSpace.Data, context)
+        {
+            Contract.Requires(optionalWhiteSpace != null);
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/BadWhiteSpaceLexer.cs
@@ -0,0 +1,38 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class BadWhiteSpaceLexer : Lexer<BadWhiteSpace>
+    {
+        private readonly ILexer<OptionalWhiteSpace> optionalWhiteSpaceLexer;
+
+        public BadWhiteSpaceLexer(ILexer<OptionalWhiteSpace> optionalWhiteSpaceLexer)
+            : base("BWS")
+        {
+            Contract.Requires(optionalWhiteSpaceLexer != null);
+            this.optionalWhiteSpaceLexer = optionalWhiteSpaceLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out BadWhiteSpace element)
+        {
+            var context = scanner.GetContext();
+            OptionalWhiteSpace optionalWhiteSpace;
+            if (this.optionalWhiteSpaceLexer.TryRead(scanner, out optionalWhiteSpace))
+            {
+                element = new BadWhiteSpace(optionalWhiteSpace, context);
+                return true;
+            }
+
+            element = default(BadWhiteSpace);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.optionalWhiteSpaceLexer != null);
+        }
+    }
+}
