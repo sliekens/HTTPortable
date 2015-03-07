cb56766bc4f7ff08f9063b26a53be4bdbc19a199
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 18:28:34 +0100

    Adding support for 'qdtext' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/QuotedText.cs
@@ -0,0 +1,55 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class QuotedText : Element
+    {
+        private QuotedText(char data, ITextContext context)
+            : base(data, context)
+        {
+        }
+
+        private QuotedText(string data, ITextContext context)
+            : base(data, context)
+        {
+        }
+
+        public static QuotedText Create1(HorizontalTab horizontalTab, ITextContext context)
+        {
+            Contract.Requires(horizontalTab != null);
+            Contract.Requires(context != null);
+            return new QuotedText(horizontalTab.Data, context);
+        }
+
+        public static QuotedText Create2(Space space, ITextContext context)
+        {
+            Contract.Requires(space != null);
+            Contract.Requires(context != null);
+            return new QuotedText(space.Data, context);
+        }
+
+        public static QuotedText Create3(char terminal, ITextContext context)
+        {
+            Contract.Requires(terminal == '!');
+            Contract.Requires(context != null);
+            return new QuotedText(terminal, context);
+        }
+
+        public static QuotedText Create4(char terminal, ITextContext context)
+        {
+            Contract.Requires(terminal >= '\x0023' && terminal <= '\x005B');
+            Contract.Requires(context != null);
+            return new QuotedText(terminal, context);
+        }
+
+        public static QuotedText Create5(char terminal, ITextContext context)
+        {
+            Contract.Requires(terminal >= '\x005D' && terminal <= '\x007E');
+            Contract.Requires(context != null);
+            return new QuotedText(terminal, context);
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/QuotedTextLexer.cs
@@ -0,0 +1,87 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class QuotedTextLexer : Lexer<QuotedText>
+    {
+        private readonly ILexer<HorizontalTab> horizontalTabLexer;
+
+        private readonly ILexer<Space> spaceLexer;
+
+        public QuotedTextLexer()
+            : this(new HorizontalTabLexer(), new SpaceLexer())
+        {
+        }
+
+        public QuotedTextLexer(ILexer<HorizontalTab> horizontalTabLexer, ILexer<Space> spaceLexer)
+            : base("qdtext")
+        {
+            Contract.Requires(horizontalTabLexer != null);
+            Contract.Requires(spaceLexer != null);
+            this.horizontalTabLexer = horizontalTabLexer;
+            this.spaceLexer = spaceLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out QuotedText element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(QuotedText);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            HorizontalTab horizontalTab;
+            if (this.horizontalTabLexer.TryRead(scanner, out horizontalTab))
+            {
+                element = QuotedText.Create1(horizontalTab, context);
+                return true;
+            }
+
+            Space space;
+            if (this.spaceLexer.TryRead(scanner, out space))
+            {
+                element = QuotedText.Create2(space, context);
+                return true;
+            }
+
+            char c = '!';
+            if (scanner.TryMatch(c))
+            {
+                element = QuotedText.Create3(c, context);
+                return true;
+            }
+
+            for (c = '\x0023'; c < '\x005B'; c++)
+            {
+                if (scanner.TryMatch(c))
+                {
+                    element = QuotedText.Create4(c, context);
+                    return true;
+                }
+            }
+
+            for (c = '\x005D'; c < '\x007E'; c++)
+            {
+                if (scanner.TryMatch(c))
+                {
+                    element = QuotedText.Create5(c, context);
+                    return true;
+                }
+            }
+
+            element = default(QuotedText);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.horizontalTabLexer != null);
+            Contract.Invariant(this.spaceLexer != null);
+        }
+    }
+}
