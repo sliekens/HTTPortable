46416b4afe93dd5bb0d1b5d42b994d5e3ce3f4e1
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 14:33:37 +0100

    Adding support for 'absolute-URI' elements (RFC 3986)

---
+++ src/Uri.Grammar/AbsoluteUri.cs
@@ -0,0 +1,66 @@
+﻿namespace Uri.Grammar
+{
+    using System.Diagnostics;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class AbsoluteUri : Element
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly Scheme scheme;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly HierarchicalPart hierarchicalPart;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly Query query;
+
+        public AbsoluteUri(Scheme scheme, Element schemeSeparator, HierarchicalPart hierarchicalPart, Element querySeparator, Query query, ITextContext context)
+            : base(string.Concat(scheme, schemeSeparator, hierarchicalPart, querySeparator, query), context)
+        {
+            Contract.Requires(scheme != null);
+            Contract.Requires(schemeSeparator != null && schemeSeparator.Data == ":");
+            Contract.Requires(hierarchicalPart != null);
+            Contract.Requires(querySeparator == null || (querySeparator.Data == "?" && query != null));
+            Contract.Requires(context != null);
+            this.scheme = scheme;
+            this.hierarchicalPart = hierarchicalPart;
+            this.query = query;
+        }
+
+        /// <summary>Gets the scheme. The name of the scheme refers to a specification for assigning identifiers within the scheme.</summary>
+        public Scheme Scheme
+        {
+            get
+            {
+                return this.scheme;
+            }
+        }
+
+        /// <summary>Gets the hierarchical part that represents the scheme.</summary>
+        public HierarchicalPart HierarchicalPart
+        {
+            get
+            {
+                return this.hierarchicalPart;
+            }
+        }
+
+        /// <summary>Gets the query component. The query serves to identify a resource within the scope of the URI's scheme and naming authority (if any).</summary>
+        public Query Query
+        {
+            get
+            {
+                return this.query;
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.scheme != null);
+            Contract.Invariant(this.hierarchicalPart != null);
+        }
+    }
+}
---
+++ src/Uri.Grammar/AbsoluteUriLexer.cs
@@ -0,0 +1,114 @@
+﻿namespace Uri.Grammar
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using QueryPart = Text.Scanning.Sequence<Text.Scanning.Element, Query>;
+
+    public class AbsoluteUriLexer : Lexer<AbsoluteUri>
+    {
+        private readonly ILexer<Scheme> schemeLexer;
+
+        private readonly ILexer<HierarchicalPart> hierarchicalPartLexer;
+
+        private readonly ILexer<Query> queryLexer;
+
+        public AbsoluteUriLexer()
+            : this(new SchemeLexer(), new HierarchicalPartLexer(), new QueryLexer())
+        {
+        }
+
+        public AbsoluteUriLexer(ILexer<Scheme> schemeLexer, ILexer<HierarchicalPart> hierarchicalPartLexer, ILexer<Query> queryLexer)
+            : base("absolute-URI")
+        {
+            Contract.Requires(schemeLexer != null);
+            Contract.Requires(hierarchicalPartLexer != null);
+            Contract.Requires(queryLexer != null);
+            this.schemeLexer = schemeLexer;
+            this.hierarchicalPartLexer = hierarchicalPartLexer;
+            this.queryLexer = queryLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out AbsoluteUri element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(AbsoluteUri);
+                return false;
+            }
+
+            Scheme scheme;
+            Element schemeSeparator;
+            HierarchicalPart hierarchicalPart;
+            Element querySeparator = default(Element);
+            Query query = default(Query);
+            var context = scanner.GetContext();
+            if (!this.schemeLexer.TryRead(scanner, out scheme))
+            {
+                element = default(AbsoluteUri);
+                return false;
+            }
+
+            if (!TryReadTerminal(scanner, ':', out schemeSeparator))
+            {
+                scanner.PutBack(scheme.Data);
+                element = default(AbsoluteUri);
+                return false;
+            }
+
+            if (!this.hierarchicalPartLexer.TryRead(scanner, out hierarchicalPart))
+            {
+                scanner.PutBack(schemeSeparator.Data);
+                scanner.PutBack(scheme.Data);
+                element = default(AbsoluteUri);
+                return false;
+            }
+
+            QueryPart queryPart;
+            if (this.TryReadQueryPart(scanner, out queryPart))
+            {
+                querySeparator = queryPart.Element1;
+                query = queryPart.Element2;
+            }
+
+            element = new AbsoluteUri(scheme, schemeSeparator, hierarchicalPart, querySeparator, query, context);
+            return true;
+        }
+
+        private bool TryReadQueryPart(ITextScanner scanner, out QueryPart element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(QueryPart);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Element questionMark;
+            if (!TryReadTerminal(scanner, '?', out questionMark))
+            {
+                element = default(QueryPart);
+                return false;
+            }
+
+            Query query;
+            if (!this.queryLexer.TryRead(scanner, out query))
+            {
+                scanner.PutBack(questionMark.Data);
+                element = default(QueryPart);
+                return false;
+            }
+
+            element = new QueryPart(questionMark, query, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.schemeLexer != null);
+            Contract.Invariant(this.hierarchicalPartLexer != null);
+            Contract.Invariant(this.queryLexer != null);
+        }
+    }
+}
