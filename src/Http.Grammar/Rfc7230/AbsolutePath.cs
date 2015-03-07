f0f9aa1daef87d0aad6357c5df667f9dd1fbed8c
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 14:49:08 +0100

    Adding support for 'absolute-path' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/AbsolutePath.cs
@@ -0,0 +1,39 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    using SegmentPart = Text.Scanning.Sequence<Text.Scanning.Element, Uri.Grammar.Segment>;
+
+    public class AbsolutePath : Element
+    {
+        public AbsolutePath(IList<SegmentPart> segments, ITextContext context)
+            : base(string.Concat(segments), context)
+        {
+            Contract.Requires(segments != null);
+            Contract.Requires(Contract.ForAll(segments,
+                sequence =>
+                {
+                    if (sequence == null)
+                    {
+                        return false;
+                    }
+
+                    if (sequence.Element1 == null || sequence.Element1.Data != "/")
+                    {
+                        return false;
+                    }
+
+                    if (sequence.Element2 == null)
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }));
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/AbsolutePathLexer.cs
@@ -0,0 +1,87 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    using SegmentPart = Text.Scanning.Sequence<Text.Scanning.Element, Uri.Grammar.Segment>;
+    using Uri.Grammar;
+
+    public class AbsolutePathLexer : Lexer<AbsolutePath>
+    {
+        private readonly ILexer<Segment> segmentLexer;
+
+        public AbsolutePathLexer()
+            : this(new SegmentLexer())
+        {
+        }
+
+        public AbsolutePathLexer(ILexer<Segment> segmentLexer)
+            : base("absolute-path")
+        {
+            Contract.Requires(segmentLexer != null);
+            this.segmentLexer = segmentLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out AbsolutePath element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(AbsolutePath);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            var segments = new List<SegmentPart>();
+            SegmentPart segment;
+            while (this.TryReadSegmentPart(scanner, out segment))
+            {
+                segments.Add(segment);
+            }
+
+            if (segments.Count == 0)
+            {
+                element = default(AbsolutePath);
+                return false;
+            }
+
+            element = new AbsolutePath(segments, context);
+            return true;
+        }
+
+        private bool TryReadSegmentPart(ITextScanner scanner, out SegmentPart element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(SegmentPart);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Element slash;
+            if (!TryReadTerminal(scanner, "/", out slash))
+            {
+                element = default(SegmentPart);
+                return false;
+            }
+
+            Segment segment;
+            if (!this.segmentLexer.TryRead(scanner, out segment))
+            {
+                scanner.PutBack(slash.Data);
+                element = default(SegmentPart);
+                return false;
+            }
+
+            element = new SegmentPart(slash, segment, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.segmentLexer != null);
+        }
+    }
+}
