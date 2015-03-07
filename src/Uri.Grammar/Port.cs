de6bf3adbf2808ad41e2c56022505c5e2511adb1
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 12:55:02 +0100

    Adding method 'Port.ToInt()'

--- src/Uri.Grammar/Port.cs
+++ src/Uri.Grammar/Port.cs
@@ -6,13 +6,28 @@
     using Text.Scanning;
     using Text.Scanning.Core;
 
+    /// <summary>Represents a port number.</summary>
     public class Port : Element
     {
+        /// <summary>Initializes a new instance of the <see cref="Port"/> class.</summary>
+        /// <param name="digits">The digits in the port number.</param>
+        /// <param name="context">The object that describes the context in which the text appears.</param>
         public Port(IList<Digit> digits, ITextContext context)
             : base(string.Concat(digits), context)
         {
             Contract.Requires(digits != null);
             Contract.Requires(Contract.ForAll(digits, digit => digit != null));
+        }
+
+        public int ToInt()
+        {
+            int result;
+            if (int.TryParse(this.Data, out result))
+            {
+                return result;
+            }
+
+            return 0;
         }
     }
 }
