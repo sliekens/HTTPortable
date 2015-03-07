8b804279f58bd9462a33064a3cc79927302c5006
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 14:55:49 +0100

    Adding support for 'authority-form' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/AuthorityForm.cs
@@ -0,0 +1,18 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    using Uri.Grammar;
+
+    public class AuthorityForm : Element
+    {
+        public AuthorityForm(Authority authority, ITextContext context)
+            : base(authority.Data, context)
+        {
+            Contract.Requires(authority != null);
+            Contract.Requires(context != null);
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/AuthorityFormLexer.cs
@@ -0,0 +1,46 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    using Uri.Grammar;
+
+    public class AuthorityFormLexer : Lexer<AuthorityForm>
+    {
+        private readonly ILexer<Authority> authorityLexer;
+
+        public AuthorityFormLexer(ILexer<Authority> authorityLexer)
+            : base("authority-form")
+        {
+            Contract.Requires(authorityLexer != null);
+            this.authorityLexer = authorityLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out AuthorityForm element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(AuthorityForm);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Authority authority;
+            if (this.authorityLexer.TryRead(scanner, out authority))
+            {
+                element = new AuthorityForm(authority, context);
+                return true;
+            }
+
+            element = default(AuthorityForm);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.authorityLexer != null);
+        }
+    }
+}
