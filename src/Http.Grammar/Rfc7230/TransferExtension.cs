ac8664c7e132245b899a60cb497dfffca06d63c9
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 21:43:56 +0100

    Adding support for 'transfer-extension' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/TransferExtension.cs
@@ -0,0 +1,44 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+
+    using Text.Scanning;
+    using ParameterPart = Text.Scanning.Sequence<OptionalWhiteSpace, Text.Scanning.Element, OptionalWhiteSpace, TransferParameter>;
+
+    public class TransferExtension : Element
+    {
+        private readonly Token extension;
+
+        private readonly IList<TransferParameter> parameters; 
+
+        public TransferExtension(Token extension, IList<ParameterPart> parameters, ITextContext context)
+            : base(string.Concat(extension, string.Concat(parameters)), context)
+        {
+            Contract.Requires(extension != null);
+            Contract.Requires(parameters != null);
+            Contract.Requires(Contract.ForAll(parameters, sequence => sequence != null));
+            Contract.Requires(context != null);
+            this.extension = extension;
+            this.parameters = new ReadOnlyCollection<TransferParameter>(parameters.Select(sequence => sequence.Element4).ToList());
+        }
+
+        public Token Extension
+        {
+            get
+            {
+                return this.extension;
+            }
+        }
+
+        public IList<TransferParameter> Parameters
+        {
+            get
+            {
+                return this.parameters;
+            }
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/TransferExtensionLexer.cs
@@ -0,0 +1,115 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using ParameterPart = Text.Scanning.Sequence<OptionalWhiteSpace, Text.Scanning.Element, OptionalWhiteSpace, TransferParameter>;
+
+    public class TransferExtensionLexer : Lexer<TransferExtension>
+    {
+        private readonly ILexer<Token> tokenLexer;
+
+        private readonly ILexer<OptionalWhiteSpace> optionalWhiteSpaceLexer;
+
+        private readonly ILexer<TransferParameter> transferParameterLexer;
+
+        public TransferExtensionLexer()
+            : this(new TokenLexer(), new OptionalWhiteSpaceLexer(), new TransferParameterLexer())
+        {
+        }
+
+        public TransferExtensionLexer(ILexer<Token> tokenLexer, ILexer<OptionalWhiteSpace> optionalWhiteSpaceLexer, ILexer<TransferParameter> transferParameterLexer)
+            : base("transfer-extension")
+        {
+            Contract.Requires(tokenLexer != null);
+            Contract.Requires(optionalWhiteSpaceLexer != null);
+            Contract.Requires(transferParameterLexer != null);
+            this.tokenLexer = tokenLexer;
+            this.optionalWhiteSpaceLexer = optionalWhiteSpaceLexer;
+            this.transferParameterLexer = transferParameterLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out TransferExtension element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(TransferExtension);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Token extension;
+            if (!this.tokenLexer.TryRead(scanner, out extension))
+            {
+                element = default(TransferExtension);
+                return false;
+            }
+
+            var parameters = new List<ParameterPart>();
+            ParameterPart parameter;
+            while (this.TryReadParameterPart(scanner, out parameter))
+            {
+                parameters.Add(parameter);
+            }
+
+            element = new TransferExtension(extension, parameters, context);
+            return true;
+        }
+
+        private bool TryReadParameterPart(ITextScanner scanner, out ParameterPart element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(ParameterPart);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            OptionalWhiteSpace optionalWhiteSpace1;
+            if (!this.optionalWhiteSpaceLexer.TryRead(scanner, out optionalWhiteSpace1))
+            {
+                element = default(ParameterPart);
+                return false;
+            }
+
+            Element semicolon;
+            if (!TryReadTerminal(scanner, ';', out semicolon))
+            {
+                scanner.PutBack(optionalWhiteSpace1.Data);
+                element = default(ParameterPart);
+                return false;
+            }
+
+            OptionalWhiteSpace optionalWhiteSpace2;
+            if (!this.optionalWhiteSpaceLexer.TryRead(scanner, out optionalWhiteSpace2))
+            {
+                scanner.PutBack(semicolon.Data);
+                scanner.PutBack(optionalWhiteSpace1.Data);
+                element = default(ParameterPart);
+                return false;
+            }
+
+            TransferParameter transferParameter;
+            if (!this.transferParameterLexer.TryRead(scanner, out transferParameter))
+            {
+                scanner.PutBack(transferParameter.Data);
+                scanner.PutBack(semicolon.Data);
+                scanner.PutBack(optionalWhiteSpace1.Data);
+                element = default(ParameterPart);
+                return false;
+            }
+
+            element = new ParameterPart(optionalWhiteSpace1, semicolon, optionalWhiteSpace2, transferParameter, context);
+            return true;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.tokenLexer != null);
+            Contract.Invariant(this.optionalWhiteSpaceLexer != null);
+            Contract.Invariant(this.transferParameterLexer != null);
+        }
+    }
+}
