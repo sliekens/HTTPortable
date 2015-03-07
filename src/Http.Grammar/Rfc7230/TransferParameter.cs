206068cf31a283c58e39783cf8d42e4c54c2fa19
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 21:14:12 +0100

    Adding support for 'transfer-parameter' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/TransferParameter.cs
@@ -0,0 +1,49 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class TransferParameter : Element
+    {
+        private readonly Token name;
+
+        private readonly Element value;
+
+        public TransferParameter(Token name, Token value, ITextContext context)
+            : base(string.Concat(name, "=", value), context)
+        {
+            Contract.Requires(name != null);
+            Contract.Requires(value != null);
+            Contract.Requires(context != null);
+            this.name = name;
+            this.value = value;
+        }
+
+        public TransferParameter(Token name, QuotedString value, ITextContext context)
+            : base(string.Concat(name, "=", value), context)
+        {
+            Contract.Requires(name != null);
+            Contract.Requires(value != null);
+            Contract.Requires(context != null);
+            this.name = name;
+            this.value = value;
+        }
+
+        public Token Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public Element Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/TransferParameterLexer.cs
@@ -0,0 +1,104 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class TransferParameterLexer : Lexer<TransferParameter>
+    {
+        private readonly ILexer<Token> tokenLexer;
+
+        private readonly ILexer<BadWhiteSpace> badWhiteSpaceLexer;
+
+        private readonly ILexer<QuotedString> quotedStringLexer;
+
+        public TransferParameterLexer()
+            : this(new TokenLexer(), new BadWhiteSpaceLexer(), new QuotedStringLexer())
+        {
+        }
+
+        public TransferParameterLexer(ILexer<Token> tokenLexer, ILexer<BadWhiteSpace> badWhiteSpaceLexer, ILexer<QuotedString> quotedStringLexer)
+            : base("transfer-parameter")
+        {
+            Contract.Requires(tokenLexer != null);
+            Contract.Requires(badWhiteSpaceLexer != null);
+            Contract.Requires(quotedStringLexer != null);
+            this.tokenLexer = tokenLexer;
+            this.badWhiteSpaceLexer = badWhiteSpaceLexer;
+            this.quotedStringLexer = quotedStringLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out TransferParameter element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(TransferParameter);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Token name;
+            if (!this.tokenLexer.TryRead(scanner, out name))
+            {
+                element = default(TransferParameter);
+                return false;
+            }
+
+            BadWhiteSpace badWhiteSpace1;
+            if (!this.badWhiteSpaceLexer.TryRead(scanner, out badWhiteSpace1))
+            {
+                scanner.PutBack(name.Data);
+                element = default(TransferParameter);
+                return false;
+            }
+
+            Element equalsSign;
+            if (!TryReadTerminal(scanner, '=', out equalsSign))
+            {
+                scanner.PutBack(badWhiteSpace1.Data);
+                scanner.PutBack(name.Data);
+                element = default(TransferParameter);
+                return false;
+            }
+
+            BadWhiteSpace badWhiteSpace2;
+            if (!this.badWhiteSpaceLexer.TryRead(scanner, out badWhiteSpace2))
+            {
+                scanner.PutBack(equalsSign.Data);
+                scanner.PutBack(badWhiteSpace1.Data);
+                scanner.PutBack(name.Data);
+                element = default(TransferParameter);
+                return false;
+            }
+
+            Token value;
+            if (this.tokenLexer.TryRead(scanner, out value))
+            {
+                element = new TransferParameter(name, value, context);
+                return true;
+            }
+
+            QuotedString quotedValue;
+            if (this.quotedStringLexer.TryRead(scanner, out quotedValue))
+            {
+                element = new TransferParameter(name, quotedValue, context);
+                return true;
+            }
+
+            scanner.PutBack(badWhiteSpace2.Data);
+            scanner.PutBack(equalsSign.Data);
+            scanner.PutBack(badWhiteSpace1.Data);
+            scanner.PutBack(name.Data);
+            element = default(TransferParameter);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.tokenLexer != null);
+            Contract.Invariant(this.badWhiteSpaceLexer != null);
+            Contract.Invariant(this.quotedStringLexer != null);
+        }
+    }
+}
