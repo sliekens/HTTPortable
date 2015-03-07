f8cd727e5b19e306ec9926534ec6e9b8181c2380
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 14:52:54 +0100

    Adding support for 'asterisk-form' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/AsteriskForm.cs
@@ -0,0 +1,16 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    public class AsteriskForm : Element
+    {
+        public AsteriskForm(Element asterisk, ITextContext context)
+            : base(asterisk.Data, context)
+        {
+            Contract.Requires(asterisk != null && asterisk.Data == "*");
+            Contract.Requires(context != null);
+        }
+    }
+}
