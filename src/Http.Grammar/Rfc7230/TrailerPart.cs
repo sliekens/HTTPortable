279ae1aafd139eabebcf189b14e3379da5ae3e1f
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 16:06:36 +0100

    Adding support for 'trailer-part' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/TrailerPart.cs
@@ -0,0 +1,39 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class TrailerPart : Element
+    {
+        private readonly IList<HeaderField> headerFields;
+
+        public TrailerPart(IList<HeaderField> headerFields, EndOfLine endOfLine, ITextContext context)
+            : base(string.Concat(string.Concat(headerFields), endOfLine), context)
+        {
+            Contract.Requires(headerFields != null);
+            Contract.Requires(headerFields.Count != 0);
+            Contract.Requires(Contract.ForAll(headerFields, field => field != null));
+            Contract.Requires(endOfLine != null);
+            Contract.Requires(context != null);
+            this.headerFields = headerFields;
+        }
+
+        public IList<HeaderField> HeaderFields
+        {
+            get
+            {
+                return new ReadOnlyCollection<HeaderField>(this.headerFields);
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.headerFields != null);
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/TrailerPartLexer.cs
@@ -0,0 +1,69 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+    using Text.Scanning.Core;
+
+    public class TrailerPartLexer : Lexer<TrailerPart>
+    {
+        private readonly ILexer<HeaderField> headerFieldLexer;
+
+        private readonly ILexer<EndOfLine> endOfLineLexer;
+
+        public TrailerPartLexer(ILexer<HeaderField> headerFieldLexer, ILexer<EndOfLine> endOfLineLexer)
+            : base("trailer-part")
+        {
+            Contract.Requires(headerFieldLexer != null);
+            Contract.Requires(endOfLineLexer != null);
+            this.headerFieldLexer = headerFieldLexer;
+            this.endOfLineLexer = endOfLineLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out TrailerPart element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(TrailerPart);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            var headerFields = new List<HeaderField>();
+            HeaderField headerField;
+            while (this.headerFieldLexer.TryRead(scanner, out headerField))
+            {
+                headerFields.Add(headerField);
+            }
+
+            if (headerFields.Count == 0)
+            {
+                element = default(TrailerPart);
+                return false;
+            }
+
+            EndOfLine endOfLine;
+            if (this.endOfLineLexer.TryRead(scanner, out endOfLine))
+            {
+                element = new TrailerPart(headerFields, endOfLine, context);
+                return true;
+            }
+
+            for (int i = headerFields.Count - 1; i >= 0; i--)
+            {
+                scanner.PutBack(headerFields[i].Data);
+            }
+
+            element = default(TrailerPart);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.headerFieldLexer != null);
+            Contract.Invariant(this.endOfLineLexer != null);
+        }
+    }
+}
