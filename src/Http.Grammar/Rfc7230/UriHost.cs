366f10f8fa52d5227f4ab6d5aa3e19d3a4c1acba
Steven Liekens <steven.liekens@gmail.com>
2015-03-07 15:51:45 +0100

    Adding support for 'uri-host' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/UriHost.cs
@@ -0,0 +1,35 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    using Uri.Grammar;
+
+    public class UriHost : Element
+    {
+        private readonly Host host;
+
+        public UriHost(Host host, ITextContext context)
+            : base(host.Data, context)
+        {
+            Contract.Requires(host != null);
+            Contract.Requires(context != null);
+            this.host = host;
+        }
+
+        public Host Host
+        {
+            get
+            {
+                return this.host;
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.host != null);
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/UriHostLexer.cs
@@ -0,0 +1,51 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+
+    using Text.Scanning;
+
+    using Uri.Grammar;
+
+    public class UriHostLexer : Lexer<UriHost>
+    {
+        private readonly ILexer<Host> hostLexer;
+
+        public UriHostLexer()
+            : this(new HostLexer())
+        {
+        }
+
+        public UriHostLexer(ILexer<Host> hostLexer)
+            : base("uri-host")
+        {
+            Contract.Requires(hostLexer != null);
+            this.hostLexer = hostLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out UriHost element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(UriHost);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Host host;
+            if (this.hostLexer.TryRead(scanner, out host))
+            {
+                element = new UriHost(host, context);
+                return true;
+            }
+
+            element = default(UriHost);
+            return false;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.hostLexer != null);
+        }
+    }
+}
