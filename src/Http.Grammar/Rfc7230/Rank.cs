ca5a3b54c2816ef1da50103abc74fcda1e4eb808
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 11:35:08 +0100

    Adding support for 'rank' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/Rank.cs
@@ -0,0 +1,24 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+    using SLANG;
+    using LessThanMaximum = SLANG.Sequence<SLANG.Element, SLANG.Option<SLANG.Sequence<SLANG.Element, SLANG.Repetition<SLANG.Core.Digit>>>>;
+    using Maximum = SLANG.Sequence<SLANG.Element, SLANG.Option<SLANG.Sequence<SLANG.Element, SLANG.Repetition<SLANG.Element>>>>;
+
+    public class Rank : Alternative<LessThanMaximum, Maximum>
+    {
+        public Rank(LessThanMaximum element, ITextContext context)
+            : base(element, context)
+        {
+            Contract.Requires(element != null);
+            Contract.Requires(context != null);
+        }
+
+        public Rank(Maximum element, ITextContext context)
+            : base(element, context)
+        {
+            Contract.Requires(element != null);
+            Contract.Requires(context != null);
+        }
+    }
+}
