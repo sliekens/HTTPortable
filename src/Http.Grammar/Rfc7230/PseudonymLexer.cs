6da3eee648ea999ada6ea7a00614b3986c8668e4
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 13:48:22 +0100

    Adding support for 'pseudonym' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/PseudonymLexer.cs
@@ -0,0 +1,33 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+    using Pseudonym = Token;
+
+    class PseudonymLexer : Lexer<Pseudonym>
+    {
+        private readonly ILexer<Token> tokenLexer;
+
+        public PseudonymLexer(ILexer<Token> tokenLexer)
+            : base("pseudonym")
+        {
+            this.tokenLexer = tokenLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out Pseudonym element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(Pseudonym);
+                return false;
+            }
+
+            if (this.tokenLexer.TryRead(scanner, out element))
+            {
+                return true;
+            }
+
+            element = default(Pseudonym);
+            return false;
+        }
+    }
+}
