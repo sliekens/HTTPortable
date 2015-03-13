4c604346c93657accf3486709f8d96258ef2e0e4
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 13:54:23 +0100

    Adding support for 'protocol-name' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ProtocolNameLexer.cs
@@ -0,0 +1,22 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+
+    using SLANG;
+    using ProtocolName = Token;
+
+    public class ProtocolNameLexer : Lexer<ProtocolName>
+    {
+        private readonly ILexer<Token> tokenLexer;
+
+        public ProtocolNameLexer(ILexer<Token> tokenLexer)
+            : base("protocol-name")
+        {
+            this.tokenLexer = tokenLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out ProtocolName element)
+        {
+            return this.tokenLexer.TryRead(scanner, out element);
+        }
+    }
+}
