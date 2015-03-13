044bb2f847da20a0c5cb6543167a2b57c46255f9
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 14:32:23 +0100

    Changing 'OriginForm' to derive from 'Sequence'

--- src/Http.Grammar/Rfc7230/OriginFormLexer.cs
+++ src/Http.Grammar/Rfc7230/OriginFormLexer.cs
@@ -39,20 +39,59 @@
                 return false;
             }
 
-            Element querySeparator;
-            if (!TryReadTerminal(scanner, '?', out querySeparator))
+            Option<Sequence<Element, Query>> optionalQuery;
+            if (!this.TryReadOptionalQuery(scanner, out optionalQuery))
             {
-                element = new OriginForm(absolutePath, null, null, context);
-                return true;
+                scanner.PutBack(absolutePath.Data);
+                element = default(OriginForm);
+                return false;
+            }
+
+            element = new OriginForm(absolutePath, optionalQuery, context);
+            return true;
+        }
+
+        private bool TryReadOptionalQuery(ITextScanner scanner, out Option<Sequence<Element, Query>> element)
+        {
+            var context = scanner.GetContext();
+            Sequence<Element, Query> queryPart;
+            if (this.TryReadQueryPart(scanner, out queryPart))
+            {
+                element = new Option<Sequence<Element, Query>>(queryPart, context);
+            }
+            else
+            {
+                element = new Option<Sequence<Element, Query>>(context);
+            }
+
+            return true;
+        }
+
+        private bool TryReadQueryPart(ITextScanner scanner, out Sequence<Element, Query> element)
+        {
+            if (scanner.EndOfInput)
+            {
+                element = default(Sequence<Element, Query>);
+                return false;
+            }
+
+            var context = scanner.GetContext();
+            Element separator;
+            if (!TryReadTerminal(scanner, @"?", out separator))
+            {
+                element = default(Sequence<Element, Query>);
+                return false;
             }
 
             Query query;
             if (!this.queryLexer.TryRead(scanner, out query))
             {
-                scanner.PutBack(querySeparator.Data);
+                scanner.PutBack(separator.Data);
+                element = default(Sequence<Element, Query>);
+                return false;
             }
 
-            element = new OriginForm(absolutePath, querySeparator, query, context);
+            element = new Sequence<Element, Query>(separator, query, context);
             return true;
         }
 
