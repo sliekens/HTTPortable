0f8fbb455a96eb07c5ba05d0afb857e539928620
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 13:45:44 +0100

    Adding support for 't-codings' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/TransferCodingListItem.cs
@@ -0,0 +1,22 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Diagnostics.Contracts;
+    using SLANG;
+
+    public class TransferCodingListItem : Alternative<Element, Sequence<TransferCoding, Option<TransferCodingRanking>>>
+    {
+        public TransferCodingListItem(Element element, ITextContext context)
+            : base(element, context)
+        {
+            Contract.Requires(element != null);
+            Contract.Requires(context != null);
+        }
+
+        public TransferCodingListItem(Sequence<TransferCoding, Option<TransferCodingRanking>> element, ITextContext context)
+            : base(element, context)
+        {
+            Contract.Requires(element != null);
+            Contract.Requires(context != null);
+        }
+    }
+}
