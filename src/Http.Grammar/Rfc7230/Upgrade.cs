f95be7b6afa569486375e522cd46431cd9fc3d4e
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 15:39:08 +0100

    Adding support for 'Upgrade' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/Upgrade.cs
@@ -0,0 +1,12 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    public class Upgrade : Sequence<Repetition<Sequence<Element, OptionalWhiteSpace>>, Protocol, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, Protocol>>>>>
+    {
+        public Upgrade(Repetition<Sequence<Element, OptionalWhiteSpace>> element1, Protocol element2, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, Protocol>>>> element3, ITextContext context)
+            : base(element1, element2, element3, context)
+        {
+        }
+    }
+}
