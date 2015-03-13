612048885d57da8062d268bed3aecd0e14285785
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 13:56:42 +0100

    Adding support for 'protocol-version' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ProtocolVersionLexer.cs
@@ -0,0 +1,21 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+    using ProtocolVersion = Token;
+
+    public class ProtocolVersionLexer : Lexer<ProtocolVersion>
+    {
+        private readonly ILexer<Token> tokenLexer;
+
+        public ProtocolVersionLexer(ILexer<Token> tokenLexer)
+            : base("protocol-version")
+        {
+            this.tokenLexer = tokenLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out Token element)
+        {
+            return this.tokenLexer.TryRead(scanner, out element);
+        }
+    }
+}
