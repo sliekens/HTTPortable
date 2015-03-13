2f084d59da8f29e9647bce2617d0a76baaaf387e
Steven Liekens <steven.liekens@gmail.com>
2015-03-13 16:03:20 +0100

    Adding support for 'connection-option' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ConnectionOptionLexer.cs
@@ -0,0 +1,21 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+    using ConnectionOption = Token;
+
+    public class ConnectionOptionLexer : Lexer<ConnectionOption>
+    {
+        private readonly ILexer<Token> tokenLexer;
+
+        public ConnectionOptionLexer(ILexer<Token> tokenLexer)
+            : base("connection-option")
+        {
+            this.tokenLexer = tokenLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out ConnectionOption element)
+        {
+            return this.tokenLexer.TryRead(scanner, out element);
+        }
+    }
+}
