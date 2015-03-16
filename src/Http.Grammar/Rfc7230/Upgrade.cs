61bf109ea58ed45f6c85fc0b203c7d2dacf879f8
Steven Liekens <steven.liekens@gmail.com>
2015-03-16 22:12:38 +0100

    Changing 'Upgrade' to derive from 'ElementList3'

--- src/Http.Grammar/Rfc7230/Upgrade.cs
+++ src/Http.Grammar/Rfc7230/Upgrade.cs
@@ -2,7 +2,7 @@
 {
     using SLANG;
 
-    public class Upgrade : Sequence<Repetition<Sequence<Element, OptionalWhiteSpace>>, Protocol, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, Protocol>>>>>
+    public class Upgrade : ElementList3<Protocol>
     {
         public Upgrade(Repetition<Sequence<Element, OptionalWhiteSpace>> element1, Protocol element2, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, Protocol>>>> element3, ITextContext context)
             : base(element1, element2, element3, context)
