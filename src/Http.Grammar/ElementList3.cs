37eeb64e34e7c1826590e0d43844c2d760152500
Steven Liekens <steven.liekens@gmail.com>
2015-03-16 22:04:54 +0100

    Adding experimental support for lenient 1#element lexers The 'ElementList3' class is similar to the 'ElementList2' class, but it requires at least one (1) non-empty list item.
    Grammar:
    1#element = *( "," OWS ) element *( OWS "," [ OWS element ] )

---
+++ src/Http.Grammar/ElementList3.cs
@@ -0,0 +1,15 @@
+﻿namespace Http.Grammar
+{
+    using Http.Grammar.Rfc7230;
+
+    using SLANG;
+
+    public class ElementList3<T> : Sequence<Repetition<Sequence<Element, OptionalWhiteSpace>>, T, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, T>>>>>
+        where T : Element
+    {
+        public ElementList3(Repetition<Sequence<Element, OptionalWhiteSpace>> element1, T element2, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, T>>>> element3, ITextContext context)
+            : base(element1, element2, element3, context)
+        {
+        }
+    }
+}
