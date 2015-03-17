9c619f8e8522fcef353637049fa1c80f6a2faaa0
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 18:33:01 +0100

    Adding support for 'received-protocol' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ReceivedProtocol.cs
@@ -0,0 +1,14 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+    using ProtocolName = Token;
+    using ProtocolVersion = Token;
+
+    public class ReceivedProtocol : Sequence<Option<Sequence<ProtocolName, Element>>, ProtocolVersion>
+    {
+        public ReceivedProtocol(Option<Sequence<Token, Element>> element1, Token element2, ITextContext context)
+            : base(element1, element2, context)
+        {
+        }
+    }
+}
