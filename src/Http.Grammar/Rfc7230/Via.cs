370e2a23b03851cd66bbb6c062fa12c33fe77d4a
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 19:29:18 +0100

    Adding element 'Via'

---
+++ src/Http.Grammar/Rfc7230/Via.cs
@@ -0,0 +1,12 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    public class Via : ElementList3<Sequence<ReceivedProtocol, RequiredWhiteSpace, ReceivedBy, Option<Sequence<RequiredWhiteSpace, Comment>>>>
+    {
+        public Via(Repetition<Sequence<Element, OptionalWhiteSpace>> element1, Sequence<ReceivedProtocol, RequiredWhiteSpace, ReceivedBy, Option<Sequence<RequiredWhiteSpace, Comment>>> element2, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, Sequence<ReceivedProtocol, RequiredWhiteSpace, ReceivedBy, Option<Sequence<RequiredWhiteSpace, Comment>>>>>>> element3, ITextContext context)
+            : base(element1, element2, element3, context)
+        {
+        }
+    }
+}
