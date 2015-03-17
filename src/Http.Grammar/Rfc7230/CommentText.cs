6000fc6c9ab8459fff8014a5e587479f3f91c062
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 19:27:09 +0100

    Adding element 'CommentText'

---
+++ src/Http.Grammar/Rfc7230/CommentText.cs
@@ -0,0 +1,28 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+    using SLANG.Core;
+
+    public class CommentText : Alternative<HorizontalTab, Space, Element, ObsoletedText>
+    {
+        public CommentText(HorizontalTab element, ITextContext context)
+            : base(element, context)
+        {
+        }
+
+        public CommentText(Space element, ITextContext context)
+            : base(element, context)
+        {
+        }
+
+        public CommentText(Element element, ITextContext context)
+            : base(element, context)
+        {
+        }
+
+        public CommentText(ObsoletedText element, ITextContext context)
+            : base(element, context)
+        {
+        }
+    }
+}
