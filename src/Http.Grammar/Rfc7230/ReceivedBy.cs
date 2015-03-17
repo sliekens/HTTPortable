95cd503cdfa461451a755d2820780cf5c7205625
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 19:19:04 +0100

    Adding support for 'received-by' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ReceivedBy.cs
@@ -0,0 +1,21 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    using Uri.Grammar;
+    using UriHost = global::Uri.Grammar.Host;
+    using Pseudonym = Token;
+
+    public class ReceivedBy : Alternative<Sequence<UriHost, Option<Sequence<Element, Port>>>, Pseudonym>
+    {
+        public ReceivedBy(Sequence<UriHost, Option<Sequence<Element, Port>>> element, ITextContext context)
+            : base(element, context)
+        {
+        }
+
+        public ReceivedBy(Pseudonym element, ITextContext context)
+            : base(element, context)
+        {
+        }
+    }
+}
