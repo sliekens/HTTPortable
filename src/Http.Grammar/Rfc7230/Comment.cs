7cfc6179c8b604d65b100f4ec6ab7d9209bdf0f7
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 19:27:50 +0100

    Adding element 'Comment'

---
+++ src/Http.Grammar/Rfc7230/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    public class Comment : Sequence<Element, Repetition<Sequence<CommentText, QuotedPair, Comment>>, Element>
+    {
+        public Comment(Element element1, Repetition<Sequence<CommentText, QuotedPair, Comment>> element2, Element element3, ITextContext context)
+            : base(element1, element2, element3, context)
+        {
+        }
+    }
+}
