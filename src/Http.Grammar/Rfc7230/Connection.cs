8c5b2711de0690f4f1107c7826f12747c4daf63d
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 18:02:33 +0100

    Adding support for 'Connection' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/Connection.cs
@@ -0,0 +1,14 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    using ConnectionOption = Token;
+
+    public class Connection : ElementList3<ConnectionOption>
+    {
+        public Connection(Repetition<Sequence<Element, OptionalWhiteSpace>> element1, ConnectionOption element2, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, ConnectionOption>>>> element3, ITextContext context)
+            : base(element1, element2, element3, context)
+        {
+        }
+    }
+}
---
+++ src/Http.Grammar/Rfc7230/ConnectionLexer.cs
@@ -0,0 +1,24 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    using ConnectionOption = Token;
+
+    public class ConnectionLexer : ElementList3Lexer<ConnectionOption>
+    {
+        public ConnectionLexer()
+            : base("Connection", new ConnectionOptionLexer())
+        {
+        }
+
+        public ConnectionLexer(ILexer<ConnectionOption> elementLexer)
+            : base("Connection", elementLexer)
+        {
+        }
+
+        public ConnectionLexer(ILexer<OptionalWhiteSpace> optionalWhiteSpaceLexer, ILexer<ConnectionOption> elementLexer)
+            : base("Connection", optionalWhiteSpaceLexer, elementLexer)
+        {
+        }
+    }
+}
