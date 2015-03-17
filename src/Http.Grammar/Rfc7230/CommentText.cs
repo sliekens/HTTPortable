fac6ecbee903b38abcce83a68a8fd6a730e897ac
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 22:04:33 +0100

    Adding support for 'ctext' elements (RFC 7230)

--- src/Http.Grammar/Rfc7230/CommentText.cs
+++ src/Http.Grammar/Rfc7230/CommentText.cs
@@ -3,7 +3,7 @@
     using SLANG;
     using SLANG.Core;
 
-    public class CommentText : Alternative<HorizontalTab, Space, Element, ObsoletedText>
+    public partial class CommentText : Alternative<HorizontalTab, Space, CommentText.Element3, CommentText.Element4, CommentText.Element5, ObsoletedText>
     {
         public CommentText(HorizontalTab element, ITextContext context)
             : base(element, context)
@@ -15,7 +15,17 @@
         {
         }
 
-        public CommentText(Element element, ITextContext context)
+        public CommentText(Element3 element, ITextContext context)
+            : base(element, context)
+        {
+        }
+
+        public CommentText(Element4 element, ITextContext context)
+            : base(element, context)
+        {
+        }
+
+        public CommentText(Element5 element, ITextContext context)
             : base(element, context)
         {
         }
@@ -23,6 +33,54 @@
         public CommentText(ObsoletedText element, ITextContext context)
             : base(element, context)
         {
+        }
+    }
+
+    public partial class CommentText
+    {
+        public class Element3 : Element
+        {
+            public Element3(char data, ITextContext context)
+                : base(data, context)
+            {
+            }
+
+            public Element3(string data, ITextContext context)
+                : base(data, context)
+            {
+            }
+        }
+    }
+
+    public partial class CommentText
+    {
+        public class Element4 : Element
+        {
+            public Element4(char data, ITextContext context)
+                : base(data, context)
+            {
+            }
+
+            public Element4(string data, ITextContext context)
+                : base(data, context)
+            {
+            }
+        }
+    }
+    
+    public partial class CommentText
+    {
+        public class Element5 : Element
+        {
+            public Element5(char data, ITextContext context)
+                : base(data, context)
+            {
+            }
+
+            public Element5(string data, ITextContext context)
+                : base(data, context)
+            {
+            }
         }
     }
 }
