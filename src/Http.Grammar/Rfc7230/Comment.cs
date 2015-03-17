a07d7f766fe7e5d498b9e51b6c7ea16658ed79cc
Steven Liekens <steven.liekens@gmail.com>
2015-03-17 22:17:59 +0100

    Adding support for 'comment' elements (RFC 7230)

--- src/Http.Grammar/Rfc7230/Comment.cs
+++ src/Http.Grammar/Rfc7230/Comment.cs
@@ -2,11 +2,43 @@
 {
     using SLANG;
 
-    public class Comment : Sequence<Element, Repetition<Sequence<CommentText, QuotedPair, Comment>>, Element>
+    public partial class Comment : Sequence<Comment.Element1, Repetition<Sequence<CommentText, QuotedPair, Comment>>, CommentText.Element3>
     {
-        public Comment(Element element1, Repetition<Sequence<CommentText, QuotedPair, Comment>> element2, Element element3, ITextContext context)
+        public Comment(Element1 element1, Repetition<Sequence<CommentText, QuotedPair, Comment>> element2, CommentText.Element3 element3, ITextContext context)
             : base(element1, element2, element3, context)
         {
+        }
+    }
+
+    public partial class Comment
+    {
+        public class Element1 : Element
+        {
+            public Element1(char data, ITextContext context)
+                : base(data, context)
+            {
+            }
+
+            public Element1(string data, ITextContext context)
+                : base(data, context)
+            {
+            }
+        }
+    }
+
+    public partial class Comment
+    {
+        public class Element3 : Element
+        {
+            public Element3(char data, ITextContext context)
+                : base(data, context)
+            {
+            }
+
+            public Element3(string data, ITextContext context)
+                : base(data, context)
+            {
+            }
         }
     }
 }
---
+++ src/Http.Grammar/Rfc7230/CommentLexer.cs
@@ -0,0 +1,163 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+
+    using SLANG;
+
+    public partial class CommentLexer : SequenceLexer<Comment, Comment.Element1, Repetition<Sequence<CommentText, QuotedPair, Comment>>, CommentText.Element3>
+    {
+        private readonly ILexer<Comment.Element1> element1Lexer;
+
+        private readonly ILexer<Repetition<Sequence<CommentText, QuotedPair, Comment>>> element2Lexer;
+
+        private readonly ILexer<CommentText.Element3> element3Lexer;
+
+        public CommentLexer(
+            ILexer<Comment.Element1> element1Lexer,
+            ILexer<Repetition<Sequence<CommentText, QuotedPair, Comment>>> element2Lexer,
+            ILexer<CommentText.Element3> element3Lexer)
+            : base("comment")
+        {
+            this.element1Lexer = element1Lexer;
+            this.element2Lexer = element2Lexer;
+            this.element3Lexer = element3Lexer;
+        }
+
+        protected override Comment CreateInstance(Comment.Element1 element1, Repetition<Sequence<CommentText, QuotedPair, Comment>> element2, CommentText.Element3 element3, ITextContext context)
+        {
+            return new Comment(element1, element2, element3, context);
+        }
+
+        protected override bool TryRead1(ITextScanner scanner, out Comment.Element1 element)
+        {
+            return this.element1Lexer.TryRead(scanner, out element);
+        }
+
+        protected override bool TryRead2(ITextScanner scanner, out Repetition<Sequence<CommentText, QuotedPair, Comment>> element)
+        {
+            return this.element2Lexer.TryRead(scanner, out element);
+        }
+
+        protected override bool TryRead3(ITextScanner scanner, out CommentText.Element3 element)
+        {
+            return this.element3Lexer.TryRead(scanner, out element);
+        }
+    }
+
+    public partial class CommentLexer
+    {
+        public class Element1Lexer : Lexer<Comment.Element1>
+        {
+            public Element1Lexer()
+                : base("anonymous")
+            {
+            }
+
+            public override bool TryRead(ITextScanner scanner, out Comment.Element1 element)
+            {
+                var context = scanner.GetContext();
+                Element terminal;
+                if (TryReadTerminal(scanner, @"(", out terminal))
+                {
+                    element = new Comment.Element1(terminal.Data, context);
+                    return true;
+                }
+
+                element = default(Comment.Element1);
+                return false;
+            }
+        }
+    }
+
+    public partial class CommentLexer
+    {
+        public partial class Element2Lexer : RepetitionLexer<Repetition<Sequence<CommentText, QuotedPair, Comment>>, Sequence<CommentText, QuotedPair, Comment>>
+        {
+            private readonly ILexer<Sequence<CommentText, QuotedPair, Comment>> elementLexer;
+
+            public Element2Lexer(ILexer<Sequence<CommentText, QuotedPair, Comment>> elementLexer)
+                : base("anonymous", 0, int.MaxValue)
+            {
+                this.elementLexer = elementLexer;
+            }
+
+            protected override Repetition<Sequence<CommentText, QuotedPair, Comment>> CreateInstance(IList<Sequence<CommentText, QuotedPair, Comment>> elements, int lowerBound, int upperBound, ITextContext context)
+            {
+                return new Repetition<Sequence<CommentText, QuotedPair, Comment>>(elements, lowerBound, upperBound, context);
+            }
+
+            protected override bool TryRead(ITextScanner scanner, int lowerBound, int upperBound, int current, out Sequence<CommentText, QuotedPair, Comment> element)
+            {
+                return this.elementLexer.TryRead(scanner, out element);
+            }
+        }
+    }
+
+    public partial class CommentLexer
+    {
+        public partial class Element2Lexer
+        {
+            public partial class ElementLexer : SequenceLexer<Sequence<CommentText, QuotedPair, Comment>, CommentText, QuotedPair, Comment>
+            {
+                private readonly ILexer<CommentText> element1Lexer;
+
+                private readonly ILexer<QuotedPair> element2Lexer;
+
+                private readonly ILexer<Comment> element3Lexer;
+
+                public ElementLexer(ILexer<CommentText> element1Lexer, ILexer<QuotedPair> element2Lexer, ILexer<Comment> element3Lexer)
+                    : base("anonymous")
+                {
+                    this.element1Lexer = element1Lexer;
+                    this.element2Lexer = element2Lexer;
+                    this.element3Lexer = element3Lexer;
+                }
+
+                protected override Sequence<CommentText, QuotedPair, Comment> CreateInstance(CommentText element1, QuotedPair element2, Comment element3, ITextContext context)
+                {
+                    return new Sequence<CommentText, QuotedPair, Comment>(element1, element2, element3, context);
+                }
+
+                protected override bool TryRead1(ITextScanner scanner, out CommentText element)
+                {
+                    return this.element1Lexer.TryRead(scanner, out element);
+                }
+
+                protected override bool TryRead2(ITextScanner scanner, out QuotedPair element)
+                {
+                    return this.element2Lexer.TryRead(scanner, out element);
+                }
+
+                protected override bool TryRead3(ITextScanner scanner, out Comment element)
+                {
+                    return this.element3Lexer.TryRead(scanner, out element);
+                }
+            }
+        }
+    }
+
+    public partial class CommentLexer
+    {
+        public class Element3Lexer : Lexer<Comment.Element1>
+        {
+            public Element3Lexer()
+                : base("anonymous")
+            {
+            }
+
+            public override bool TryRead(ITextScanner scanner, out Comment.Element1 element)
+            {
+                var context = scanner.GetContext();
+                Element terminal;
+                if (TryReadTerminal(scanner, @")", out terminal))
+                {
+                    element = new Comment.Element1(terminal.Data, context);
+                    return true;
+                }
+
+                element = default(Comment.Element1);
+                return false;
+            }
+        }
+    }
+}
