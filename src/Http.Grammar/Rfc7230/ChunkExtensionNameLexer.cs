8f1f4dbeabc21b919dbe7445fcd0862e8a95bcba
Steven Liekens <steven.liekens@gmail.com>
2015-03-30 20:48:24 +0200

    Adding support for 'chunk-ext-name' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ChunkExtensionNameLexer.cs
@@ -0,0 +1,22 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    using ChunkExtensionName = Token;
+
+    public class ChunkExtensionNameLexer : Lexer<Token>
+    {
+        private readonly ILexer<Token> tokenLexer;
+
+        public ChunkExtensionNameLexer(ILexer<Token> tokenLexer)
+            : base("chunk-ext-name")
+        {
+            this.tokenLexer = tokenLexer;
+        }
+
+        public override bool TryRead(ITextScanner scanner, out ChunkExtensionName element)
+        {
+            return this.tokenLexer.TryRead(scanner, out element);
+        }
+    }
+}
