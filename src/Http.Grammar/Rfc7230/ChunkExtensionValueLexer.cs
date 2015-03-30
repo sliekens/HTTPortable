a362a1eeb308732d192297a19dca04e30a5e6238
Steven Liekens <steven.liekens@gmail.com>
2015-03-30 21:09:20 +0200

    Adding support for 'chunk-ext-value' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ChunkExtensionValueLexer.cs
@@ -0,0 +1,43 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using SLANG;
+
+    public class ChunkExtensionValueLexer : AlternativeLexer<ChunkExtensionValue, Token, QuotedString>
+    {
+        private readonly ILexer<Token> element1Lexer;
+
+        private readonly ILexer<QuotedString> element2Lexer;
+
+        public ChunkExtensionValueLexer()
+            : this(new TokenLexer(), new QuotedStringLexer())
+        {
+        }
+
+        public ChunkExtensionValueLexer(ILexer<Token> element1Lexer, ILexer<QuotedString> element2Lexer)
+            : base("chunk-ext-value")
+        {
+            this.element1Lexer = element1Lexer;
+            this.element2Lexer = element2Lexer;
+        }
+
+        protected override ChunkExtensionValue CreateInstance1(Token element, ITextContext context)
+        {
+            return new ChunkExtensionValue(element, 1, context);
+        }
+
+        protected override ChunkExtensionValue CreateInstance2(QuotedString element, ITextContext context)
+        {
+            return new ChunkExtensionValue(element, 2, context);
+        }
+
+        protected override bool TryRead1(ITextScanner scanner, out Token element)
+        {
+            return this.element1Lexer.TryRead(scanner, out element);
+        }
+
+        protected override bool TryRead2(ITextScanner scanner, out QuotedString element)
+        {
+            return this.element2Lexer.TryRead(scanner, out element);
+        }
+    }
+}
