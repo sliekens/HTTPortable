55dec803bcb8aa6ceb3a80f2335d6eae422f28fb
Steven Liekens <steven.liekens@gmail.com>
2015-03-30 20:15:51 +0200

    Adding class 'TransferEncodingHeader'

---
+++ src/Http/Headers/TransferEncodingHeader.cs
@@ -0,0 +1,20 @@
+﻿namespace Http.Headers
+{
+    using System.Collections.Generic;
+
+    public class TransferEncodingHeader : Header
+    {
+        public const string FieldName = "Transfer-Encoding";
+
+        public TransferEncodingHeader()
+            : base(FieldName)
+        {
+        }
+
+        public TransferEncodingHeader(IList<string> transferCodings)
+            : base(FieldName)
+        {
+            this.AddRange(transferCodings);
+        }
+    }
+}
