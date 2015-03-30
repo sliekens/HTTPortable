edc6672042c107d296667e88f9943d6aac693d48
Steven Liekens <steven.liekens@gmail.com>
2015-03-30 20:45:19 +0200

    Adding support for 'chunk-size' elements (RFC 7230)

---
+++ src/Http.Grammar/Rfc7230/ChunkSize.cs
@@ -0,0 +1,15 @@
+﻿namespace Http.Grammar.Rfc7230
+{
+    using System.Collections.Generic;
+
+    using SLANG;
+    using SLANG.Core;
+
+    public class ChunkSize : Repetition<HexadecimalDigit>
+    {
+        public ChunkSize(IList<HexadecimalDigit> elements, ITextContext context)
+            : base(elements, 1, int.MaxValue, context)
+        {
+        }
+    }
+}
