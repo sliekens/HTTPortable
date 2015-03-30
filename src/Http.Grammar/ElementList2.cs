4a59ccdf9b9d67c0075d3f87a04790580e031673
Steven Liekens <steven.liekens@gmail.com>
2015-03-30 20:14:01 +0200

    Adding helper method 'ElementList2.GetElements()'

--- src/Http.Grammar/ElementList2.cs
+++ src/Http.Grammar/ElementList2.cs
@@ -1,5 +1,8 @@
 ﻿namespace Http.Grammar
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     using Http.Grammar.Rfc7230;
 
     using SLANG;
@@ -15,6 +18,23 @@
         public ElementList2(Sequence<Alternative<Element, T>, Repetition<Sequence<OptionalWhiteSpace, Element, Option<Sequence<OptionalWhiteSpace, T>>>>> element, ITextContext context)
             : base(element, context)
         {
+        }
+
+        public IList<T> GetElements()
+        {
+            var elements = new List<T>();
+            foreach (var sequence in this.Elements)
+            {
+                var element = sequence.Element1 as T;
+                if (element != null)
+                {
+                    elements.Add(element);
+                }
+
+                elements.AddRange(sequence.Element2.Elements.SelectMany(s => s.Element3.Elements).Select(o => o.Element2));
+            }
+
+            return elements;
         }
     }
 }
