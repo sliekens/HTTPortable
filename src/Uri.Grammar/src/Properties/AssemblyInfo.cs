4470b9a4d71378643dadea450a0dd4c9627dbfe2
Steven Liekens <steven.liekens@gmail.com>
2015-04-05 00:43:12 +0200

    Reorganizing filesystem layout

---
+++ src/Uri.Grammar/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Uri.Grammar")]
+[assembly: AssemblyDescription("Implements RFC 3986")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Steven Liekens")]
+[assembly: AssemblyProduct("Uri.Grammar")]
+[assembly: AssemblyCopyright("The MIT License (MIT)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-debug")]
