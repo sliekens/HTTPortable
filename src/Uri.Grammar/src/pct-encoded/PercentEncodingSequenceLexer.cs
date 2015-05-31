f8791701807a313f11e572e420518affbfbdb736
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 15:39:49 +0200

    Adding missing file

---
+++ src/Uri.Grammar/src/pct-encoded/PercentEncodingSequenceLexer.cs
@@ -0,0 +1,22 @@
+﻿namespace Uri.Grammar
+{
+    using SLANG;
+
+    public class PercentEncodingSequenceLexer : SequenceLexer
+    {
+        /// <summary></summary>
+        /// <param name="lexers">"%" HEXDIG HEXDIG</param>
+        public PercentEncodingSequenceLexer(params ILexer[] lexers)
+            : base(lexers)
+        {
+        }
+    }
+
+    public class PercentEncoding1StringLexer : StringLexer
+    {
+        public PercentEncoding1StringLexer()
+            : base(@"%")
+        {
+        }
+    }
+}
