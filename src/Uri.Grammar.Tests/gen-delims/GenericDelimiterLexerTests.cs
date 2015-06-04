2db1f08a1ee3da7d10621d98ea638fc5f1ecc0e9
Steven Liekens <steven.liekens@gmail.com>
2015-06-04 16:08:01 +0200

    Adding GenericDelimiterLexerTests

---
+++ src/Uri.Grammar.Tests/gen-delims/GenericDelimiterLexerTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Uri.Grammar.gen_delims
+{
+    using Xunit;
+
+    public class GenericDelimiterLexerTests
+    {
+        [Theory]
+        [InlineData(@":")]
+        [InlineData(@"/")]
+        [InlineData(@"?")]
+        [InlineData(@"#")]
+        [InlineData(@"[")]
+        [InlineData(@"]")]
+        [InlineData(@"@")]
+        public void Read_ShouldSucceed(string input)
+        {
+            
+        }
+    }
+}
