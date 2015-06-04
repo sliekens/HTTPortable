edcd4e04f3021ab7fd17fb9c7b7d274c25f43688
Steven Liekens <steven.liekens@gmail.com>
2015-06-04 15:47:34 +0200

    Adding SubcomponentsDelimiterLexerTests

---
+++ src/Uri.Grammar.Tests/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Uri.Grammar
+{
+    using System.IO;
+    using System.Text;
+
+    internal static class StringExtensions
+    {
+        internal static Stream ToMemoryStream(this string instance)
+        {
+            return new MemoryStream(Encoding.GetEncoding("iso-8859-1").GetBytes(instance));
+        }
+    }
+}
---
+++ src/Uri.Grammar.Tests/sub-delims/SubcomponentsDelimiterLexerTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Uri.Grammar.sub_delims
+{
+    using SLANG;
+
+    using Xunit;
+
+    public class SubcomponentsDelimiterLexerTests
+    {
+        [Theory]
+        [InlineData(@"!")]
+        [InlineData(@"$")]
+        [InlineData(@"&")]
+        [InlineData(@"'")]
+        [InlineData(@"(")]
+        [InlineData(@")")]
+        [InlineData(@"*")]
+        [InlineData(@"+")]
+        [InlineData(@",")]
+        [InlineData(@";")]
+        [InlineData(@"=")]
+        public void Read_ShouldSucceed(string input)
+        {
+            var caseInsensitiveTerminalLexerFactory = new CaseInsensitiveTerminalLexerFactory();
+            var stringLexerFactory = new StringLexerFactory(caseInsensitiveTerminalLexerFactory);
+            var alternativeLexerFactory = new AlternativeLexerFactory();
+            var factory = new SubcomponentsDelimiterLexerFactory(stringLexerFactory, alternativeLexerFactory);
+            var lexer = factory.Create();
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                scanner.Read();
+                var element = lexer.Read(scanner);
+                Assert.NotNull(element);
+                Assert.Equal(input, element.Data);
+            }
+        }
+    }
+}
