c834f260cd28becaef934e50dfd76911a2270b04
Steven Liekens <steven.liekens@gmail.com>
2015-06-04 22:10:05 +0200

    Reimplementing 'segment'

---
+++ src/Uri.Grammar/segment/SegmentLexerFactory.cs
@@ -0,0 +1,36 @@
+﻿namespace Uri.Grammar.segment
+{
+    using System;
+
+    using SLANG;
+
+    public class SegmentLexerFactory : ILexerFactory<Segment>
+    {
+        private readonly ILexerFactory<PathCharacter> pathCharacterLexerFactory;
+
+        private readonly IRepetitionLexerFactory repetitionLexerFactory;
+
+        public SegmentLexerFactory(ILexerFactory<PathCharacter> pathCharacterLexerFactory, IRepetitionLexerFactory repetitionLexerFactory)
+        {
+            if (pathCharacterLexerFactory == null)
+            {
+                throw new ArgumentNullException("pathCharacterLexerFactory", "Precondition: pathCharacterLexerFactory != null");
+            }
+
+            if (repetitionLexerFactory == null)
+            {
+                throw new ArgumentNullException("repetitionLexerFactory", "Precondition: repetitionLexerFactory != null");
+            }
+
+            this.pathCharacterLexerFactory = pathCharacterLexerFactory;
+            this.repetitionLexerFactory = repetitionLexerFactory;
+        }
+
+        public ILexer<Segment> Create()
+        {
+            var pathCharacterLexer = this.pathCharacterLexerFactory.Create();
+            var segmentRepetitionLexer = this.repetitionLexerFactory.Create(pathCharacterLexer, 0, int.MaxValue);
+            return new SegmentLexer(segmentRepetitionLexer);
+        }
+    }
+}
