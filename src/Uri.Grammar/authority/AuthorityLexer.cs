b7950e33d4ed117d0c26f8de75f5dfb4305f6a9a
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 16:56:32 +0200

    Reimplementing 'authority'

--- src/Uri.Grammar/authority/AuthorityLexer.cs
+++ src/Uri.Grammar/authority/AuthorityLexer.cs
@@ -1,140 +1,34 @@
 ﻿namespace Uri.Grammar
 {
-    using System.Diagnostics.Contracts;
+    using System;
+
     using SLANG;
-    using PortInfo = SLANG.Sequence<SLANG.Element, Port>;
-    using UserInfo = SLANG.Sequence<UserInformation, SLANG.Element>;
 
     public class AuthorityLexer : Lexer<Authority>
     {
-        private readonly ILexer<Host> hostLexer;
-        private readonly ILexer<Port> portLexer;
-        private readonly ILexer<UserInformation> userInformationLexer;
+        private readonly ILexer<Sequence> innerLexer;
 
-        public AuthorityLexer()
-            : this(new UserInformationLexer(), new HostLexer(), new PortLexer())
+        public AuthorityLexer(ILexer<Sequence> innerLexer)
         {
-        }
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException("innerLexer");
+            }
 
-        public AuthorityLexer(ILexer<UserInformation> userInformationLexer, ILexer<Host> hostLexer, 
-            ILexer<Port> portLexer)
-            : base("authority")
-        {
-            Contract.Requires(userInformationLexer != null);
-            Contract.Requires(hostLexer != null);
-            Contract.Requires(portLexer != null);
-            this.userInformationLexer = userInformationLexer;
-            this.hostLexer = hostLexer;
-            this.portLexer = portLexer;
+            this.innerLexer = innerLexer;
         }
 
         public override bool TryRead(ITextScanner scanner, out Authority element)
         {
-            if (scanner.EndOfInput)
+            Sequence result;
+            if (this.innerLexer.TryRead(scanner, out result))
             {
-                element = default(Authority);
-                return false;
+                element = new Authority(result);
+                return true;
             }
 
-            UserInformation userInformation = default(UserInformation);
-            Element userInformationSeparator = default(Element);
-            Host host;
-            Element portSeparator = default(Element);
-            Port port = default(Port);
-            var context = scanner.GetContext();
-            UserInfo userInfo;
-            var hasUserInfo = this.TryReadOptionalUserInformation(scanner, out userInfo);
-            if (hasUserInfo)
-            {
-                userInformation = userInfo.Element1;
-                userInformationSeparator = userInfo.Element2;
-            }
-
-            if (!this.hostLexer.TryRead(scanner, out host))
-            {
-                if (hasUserInfo)
-                {
-                    scanner.PutBack(userInfo.Data);
-                }
-
-                element = default(Authority);
-                return false;
-            }
-
-            PortInfo portInfo;
-            if (this.TryReadOptionalPort(scanner, out portInfo))
-            {
-                portSeparator = portInfo.Element1;
-                port = portInfo.Element2;
-            }
-
-            element = new Authority(userInformation, userInformationSeparator, host, portSeparator, port, context);
-            return true;
-        }
-
-        [ContractInvariantMethod]
-        private void ObjectInvariant()
-        {
-            Contract.Invariant(false);
-            Contract.Invariant(this.userInformationLexer != null);
-            Contract.Invariant(this.hostLexer != null);
-            Contract.Invariant(this.portLexer != null);
-        }
-
-        private bool TryReadOptionalPort(ITextScanner scanner, out PortInfo element)
-        {
-            if (scanner.EndOfInput)
-            {
-                element = default(PortInfo);
-                return false;
-            }
-
-            var context = scanner.GetContext();
-            Element colon;
-            if (!TryReadTerminal(scanner, ':', out colon))
-            {
-                element = default(PortInfo);
-                return false;
-            }
-
-            Port port;
-            if (!this.portLexer.TryRead(scanner, out port))
-            {
-                scanner.PutBack(colon.Data);
-                element = default(PortInfo);
-                return false;
-            }
-
-            element = new PortInfo(colon, port, context);
-            return true;
-        }
-
-        private bool TryReadOptionalUserInformation(ITextScanner scanner, out UserInfo element)
-        {
-            if (scanner.EndOfInput)
-            {
-                element = default(UserInfo);
-                return false;
-            }
-
-            var context = scanner.GetContext();
-            UserInformation userInformation;
-            if (!this.userInformationLexer.TryRead(scanner, out userInformation))
-            {
-                element = default(UserInfo);
-                return false;
-            }
-
-            Element at;
-            if (!TryReadTerminal(scanner, '@', out at))
-            {
-                scanner.PutBack(userInformation.Data);
-                element = default(UserInfo);
-                return false;
-            }
-
-            element = new UserInfo(userInformation, at, context);
-            return true;
+            element = default(Authority);
+            return false;
         }
     }
 }
---
+++ src/Uri.Grammar/authority/AuthorityLexerFactory.cs
@@ -0,0 +1,82 @@
+﻿namespace Uri.Grammar.authority
+{
+    using System;
+
+    using SLANG;
+
+    public class AuthorityLexerFactory : ILexerFactory<Authority>
+    {
+        private readonly ILexerFactory<Host> hostLexerFactory;
+
+        private readonly IOptionLexerFactory optionLexerFactory;
+
+        private readonly ILexerFactory<Port> portLexerFactory;
+
+        private readonly ISequenceLexerFactory sequenceLexerFactory;
+
+        private readonly IStringLexerFactory stringLexerFactory;
+
+        private readonly ILexerFactory<UserInformation> userInformationLexerFactory;
+
+        public AuthorityLexerFactory(
+            IOptionLexerFactory optionLexerFactory,
+            ISequenceLexerFactory sequenceLexerFactory,
+            ILexerFactory<UserInformation> userInformationLexerFactory,
+            IStringLexerFactory stringLexerFactory,
+            ILexerFactory<Host> hostLexerFactory,
+            ILexerFactory<Port> portLexerFactory)
+        {
+            if (optionLexerFactory == null)
+            {
+                throw new ArgumentNullException("optionLexerFactory");
+            }
+
+            if (sequenceLexerFactory == null)
+            {
+                throw new ArgumentNullException("sequenceLexerFactory");
+            }
+
+            if (userInformationLexerFactory == null)
+            {
+                throw new ArgumentNullException("userInformationLexerFactory");
+            }
+
+            if (stringLexerFactory == null)
+            {
+                throw new ArgumentNullException("stringLexerFactory");
+            }
+
+            if (hostLexerFactory == null)
+            {
+                throw new ArgumentNullException("hostLexerFactory");
+            }
+
+            if (portLexerFactory == null)
+            {
+                throw new ArgumentNullException("portLexerFactory");
+            }
+
+            this.optionLexerFactory = optionLexerFactory;
+            this.userInformationLexerFactory = userInformationLexerFactory;
+            this.stringLexerFactory = stringLexerFactory;
+            this.hostLexerFactory = hostLexerFactory;
+            this.portLexerFactory = portLexerFactory;
+            this.sequenceLexerFactory = sequenceLexerFactory;
+        }
+
+        public ILexer<Authority> Create()
+        {
+            var userinfo = this.userInformationLexerFactory.Create();
+            var at = this.stringLexerFactory.Create(@"@");
+            var userinfoseq = this.sequenceLexerFactory.Create(userinfo, at);
+            var optuserinfo = this.optionLexerFactory.Create(userinfoseq);
+            var host = this.hostLexerFactory.Create();
+            var colon = this.stringLexerFactory.Create(@":");
+            var port = this.portLexerFactory.Create();
+            var portseq = this.sequenceLexerFactory.Create(colon, port);
+            var optport = this.optionLexerFactory.Create(portseq);
+            var innerLexer = this.sequenceLexerFactory.Create(optuserinfo, host, optport);
+            return new AuthorityLexer(innerLexer);
+        }
+    }
+}
