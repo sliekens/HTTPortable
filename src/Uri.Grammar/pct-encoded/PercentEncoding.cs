a00ffabe64bc573c9ec63d2a6243869269aab08f
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 21:27:14 +0200

    Overriding PercentEncoding.GetWellFormedText()

--- src/Uri.Grammar/pct-encoded/PercentEncoding.cs
+++ src/Uri.Grammar/pct-encoded/PercentEncoding.cs
@@ -8,5 +8,16 @@
             : base(sequence)
         {
         }
+
+        public override string GetWellFormedText()
+        {
+            /* 6.2.2.1.  Case Normalization
+             * 
+             * For all URIs, the hexadecimal digits within a percent-encoding
+             * triplet (e.g., "%3a" versus "%3A") are case-insensitive and therefore
+             * should be normalized to use uppercase letters for the digits A-F.
+             */
+            return base.GetWellFormedText().ToUpperInvariant();
+        }
     }
 }
