64107c70a1259912c18b93d8214f2df32c0920dc
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 17:44:27 +0200

    Reimplementing 'hier-part'

--- src/Uri.Grammar/hier-part/HierarchicalPartLexer.cs
+++ src/Uri.Grammar/hier-part/HierarchicalPartLexer.cs
@@ -1,148 +1,29 @@
 ﻿namespace Uri.Grammar
 {
-    using System.Diagnostics.Contracts;
+    using System;
+
     using SLANG;
 
     public class HierarchicalPartLexer : Lexer<HierarchicalPart>
     {
-        private readonly ILexer<Authority> authorityLexer;
-        private readonly ILexer<PathAbsolute> pathAbsoluteLexer;
-        private readonly ILexer<PathAbsoluteOrEmpty> pathAbsoluteOrEmptyLexer;
-        private readonly ILexer<PathRootless> pathRootlessLexer;
+        private readonly ILexer<Alternative> innerLexer;
 
-        public HierarchicalPartLexer()
-            : this(
-                new AuthorityLexer(), new PathAbsoluteOrEmptyLexer(), new PathAbsoluteLexer(), new PathRootlessLexer())
+        public HierarchicalPartLexer(ILexer<Alternative> innerLexer)
         {
-        }
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException("innerLexer");
+            }
 
-        public HierarchicalPartLexer(ILexer<Authority> authorityLexer, 
-            ILexer<PathAbsoluteOrEmpty> pathAbsoluteOrEmptyLexer, ILexer<PathAbsolute> pathAbsoluteLexer, 
-            ILexer<PathRootless> pathRootlessLexer)
-            : base("hier-part")
-        {
-            Contract.Requires(authorityLexer != null);
-            Contract.Requires(pathAbsoluteOrEmptyLexer != null);
-            Contract.Requires(pathAbsoluteLexer != null);
-            Contract.Requires(pathRootlessLexer != null);
-            this.authorityLexer = authorityLexer;
-            this.pathAbsoluteOrEmptyLexer = pathAbsoluteOrEmptyLexer;
-            this.pathAbsoluteLexer = pathAbsoluteLexer;
-            this.pathRootlessLexer = pathRootlessLexer;
+            this.innerLexer = innerLexer;
         }
 
         public override bool TryRead(ITextScanner scanner, out HierarchicalPart element)
         {
-            if (scanner.EndOfInput)
+            Alternative result;
+            if (this.innerLexer.TryRead(scanner, out result))
             {
-                element = default(HierarchicalPart);
-                return false;
-            }
-
-            var context = scanner.GetContext();
-            if (this.TryRead1(scanner, out element))
-            {
-                return true;
-            }
-
-            if (this.TryRead2(scanner, out element))
-            {
-                return true;
-            }
-
-            if (this.TryRead2(scanner, out element))
-            {
-                return true;
-            }
-
-            if (this.TryRead3(scanner, out element))
-            {
-                return true;
-            }
-
-            element = new HierarchicalPart(new PathEmpty(context), context);
-            return true;
-        }
-
-        [ContractInvariantMethod]
-        private void ObjectInvariant()
-        {
-            Contract.Invariant(this.authorityLexer != null);
-            Contract.Invariant(this.pathAbsoluteOrEmptyLexer != null);
-            Contract.Invariant(this.pathAbsoluteLexer != null);
-            Contract.Invariant(this.pathRootlessLexer != null);
-        }
-
-        private bool TryRead1(ITextScanner scanner, out HierarchicalPart element)
-        {
-            if (scanner.EndOfInput)
-            {
-                element = default(HierarchicalPart);
-                return false;
-            }
-
-            var context = scanner.GetContext();
-            Element slashes;
-            if (!TryReadTerminal(scanner, "//", out slashes))
-            {
-                element = default(HierarchicalPart);
-                return false;
-            }
-
-            Authority authority;
-            if (!this.authorityLexer.TryRead(scanner, out authority))
-            {
-                scanner.PutBack(slashes.Data);
-                element = default(HierarchicalPart);
-                return false;
-            }
-
-            PathAbsoluteOrEmpty path;
-            if (!this.pathAbsoluteOrEmptyLexer.TryRead(scanner, out path))
-            {
-                scanner.PutBack(authority.Data);
-                scanner.PutBack(slashes.Data);
-                element = default(HierarchicalPart);
-                return false;
-            }
-
-            element = new HierarchicalPart(slashes, authority, path, context);
-            return true;
-        }
-
-        private bool TryRead2(ITextScanner scanner, out HierarchicalPart element)
-        {
-            if (scanner.EndOfInput)
-            {
-                element = default(HierarchicalPart);
-                return false;
-            }
-
-            var context = scanner.GetContext();
-            PathAbsolute path;
-            if (this.pathAbsoluteLexer.TryRead(scanner, out path))
-            {
-                element = new HierarchicalPart(path, context);
-                return true;
-            }
-
-            element = default(HierarchicalPart);
-            return false;
-        }
-
-        private bool TryRead3(ITextScanner scanner, out HierarchicalPart element)
-        {
-            if (scanner.EndOfInput)
-            {
-                element = default(HierarchicalPart);
-                return false;
-            }
-
-            var context = scanner.GetContext();
-            PathRootless path;
-            if (this.pathRootlessLexer.TryRead(scanner, out path))
-            {
-                element = new HierarchicalPart(path, context);
+                element = new HierarchicalPart(result);
                 return true;
             }
 
---
+++ src/Uri.Grammar/hier-part/HierarchicalPartLexerFactory.cs
@@ -0,0 +1,99 @@
+﻿namespace Uri.Grammar.hier_part
+{
+    using System;
+
+    using SLANG;
+
+    public class HierarchicalPartLexerFactory : ILexerFactory<HierarchicalPart>
+    {
+        private readonly IAlternativeLexerFactory alternativeLexerFactory;
+
+        private readonly ILexerFactory<Authority> authorityLexerFactory;
+
+        private readonly ILexerFactory<PathAbsolute> pathAbsoluteLexerFactory;
+
+        private readonly ILexerFactory<PathAbsoluteOrEmpty> pathAbsoluteOrEmptyLexerFactory;
+
+        private readonly ILexerFactory<PathEmpty> pathEmptyLexerFactory;
+
+        private readonly ILexerFactory<PathRootless> pathRootlessLexerFactory;
+
+        private readonly ISequenceLexerFactory sequenceLexerFactory;
+
+        private readonly IStringLexerFactory stringLexerFactory;
+
+        public HierarchicalPartLexerFactory(
+            IAlternativeLexerFactory alternativeLexerFactory,
+            ILexerFactory<Authority> authorityLexerFactory,
+            ILexerFactory<PathAbsolute> pathAbsoluteLexerFactory,
+            ILexerFactory<PathAbsoluteOrEmpty> pathAbsoluteOrEmptyLexerFactory,
+            ILexerFactory<PathEmpty> pathEmptyLexerFactory,
+            ILexerFactory<PathRootless> pathRootlessLexerFactory,
+            ISequenceLexerFactory sequenceLexerFactory,
+            IStringLexerFactory stringLexerFactory)
+        {
+            if (alternativeLexerFactory == null)
+            {
+                throw new ArgumentNullException("alternativeLexerFactory");
+            }
+
+            if (authorityLexerFactory == null)
+            {
+                throw new ArgumentNullException("authorityLexerFactory");
+            }
+
+            if (pathAbsoluteLexerFactory == null)
+            {
+                throw new ArgumentNullException("pathAbsoluteLexerFactory");
+            }
+
+            if (pathAbsoluteOrEmptyLexerFactory == null)
+            {
+                throw new ArgumentNullException("pathAbsoluteOrEmptyLexerFactory");
+            }
+
+            if (pathEmptyLexerFactory == null)
+            {
+                throw new ArgumentNullException("pathEmptyLexerFactory");
+            }
+
+            if (pathRootlessLexerFactory == null)
+            {
+                throw new ArgumentNullException("pathRootlessLexerFactory");
+            }
+
+            if (sequenceLexerFactory == null)
+            {
+                throw new ArgumentNullException("sequenceLexerFactory");
+            }
+
+            if (stringLexerFactory == null)
+            {
+                throw new ArgumentNullException("stringLexerFactory");
+            }
+
+            this.alternativeLexerFactory = alternativeLexerFactory;
+            this.authorityLexerFactory = authorityLexerFactory;
+            this.pathAbsoluteLexerFactory = pathAbsoluteLexerFactory;
+            this.pathAbsoluteOrEmptyLexerFactory = pathAbsoluteOrEmptyLexerFactory;
+            this.pathEmptyLexerFactory = pathEmptyLexerFactory;
+            this.pathRootlessLexerFactory = pathRootlessLexerFactory;
+            this.sequenceLexerFactory = sequenceLexerFactory;
+            this.stringLexerFactory = stringLexerFactory;
+        }
+
+        public ILexer<HierarchicalPart> Create()
+        {
+            var delim = this.stringLexerFactory.Create(@"//");
+            var authority = this.authorityLexerFactory.Create();
+            var pathAbEmpty = this.pathAbsoluteOrEmptyLexerFactory.Create();
+            var seq = this.sequenceLexerFactory.Create(delim, authority, pathAbEmpty);
+            var pathAbsolute = this.pathAbsoluteLexerFactory.Create();
+            var pathRootless = this.pathRootlessLexerFactory.Create();
+            var pathEmpty = this.pathEmptyLexerFactory.Create();
+            var innerLexer = this.alternativeLexerFactory.Create(seq, pathAbsolute, pathRootless, pathEmpty);
+            return new HierarchicalPartLexer(innerLexer);
+
+        }
+    }
+}
