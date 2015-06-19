4f6c947e362a8ba0b171ddfa2eec345f3ca772be
Steven Liekens <steven.liekens@gmail.com>
2015-06-19 22:36:49 +0200

    Adding 'OptionalDelimitedList.GetItems()'

--- src/Http.Grammar/OptionalDelimitedList.cs
+++ src/Http.Grammar/OptionalDelimitedList.cs
@@ -1,5 +1,9 @@
 ﻿namespace Http.Grammar
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using TextFx;
     using TextFx.ABNF;
 
     public class OptionalDelimitedList : Repetition
@@ -7,6 +11,27 @@
         public OptionalDelimitedList(Repetition repetition)
             : base(repetition)
         {
+        }
+
+        public IList<Element> GetItems()
+        {
+            var items = new List<Element>();
+            foreach (var seq1 in this.Elements.Cast<Sequence>())
+            {
+                var alt1 = (Alternative)seq1.Elements[0];
+                if (alt1.Ordinal == 2)
+                {
+                    items.Add(alt1.Element);
+                }
+
+                var rep1 = (Repetition)seq1.Elements[1];
+                foreach (var opt1 in rep1.Elements.Cast<Sequence>().Select(seq2 => (Repetition)seq2.Elements[2]))
+                {
+                    items.AddRange(opt1.Elements.Cast<Sequence>().Select(seq3 => seq3.Elements[1]));
+                }
+            }
+
+            return items;
         }
     }
 }
