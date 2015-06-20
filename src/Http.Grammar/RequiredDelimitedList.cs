5bdd1264cdbd4fe1fcf4c9c6ccaf8687378bd96b
Steven Liekens <steven.liekens@gmail.com>
2015-06-20 12:15:48 +0200

    Adding 'RequiredDelimitedList.GetItems()'

--- src/Http.Grammar/RequiredDelimitedList.cs
+++ src/Http.Grammar/RequiredDelimitedList.cs
@@ -1,5 +1,9 @@
 ﻿namespace Http.Grammar
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using TextFx;
     using TextFx.ABNF;
 
     public class RequiredDelimitedList : Sequence
@@ -7,6 +11,19 @@
         public RequiredDelimitedList(Sequence sequence)
             : base(sequence)
         {
+        }
+
+        public IList<Element> GetItems()
+        {
+            var e1 = this.Elements[1];
+            var items = new List<Element> { e1 };
+            var rep1 = (Repetition)this.Elements[2];
+            foreach (var opt1 in rep1.Elements.Cast<Sequence>().Select(seq1 => (Repetition)seq1.Elements[2]))
+            {
+                items.AddRange(opt1.Elements.Cast<Sequence>().Select(seq2 => seq2.Elements[1]));
+            }
+
+            return items;
         }
     }
 }
