0953c987974f59caae2aeac1699f856964c513fe
Steven Liekens <steven.liekens@gmail.com>
2015-06-20 12:26:46 +0200

    Adding RequiredDelimitedListLexerTests

---
+++ Http.Grammar.Tests/RequiredDelimitedListTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Http.Grammar.Tests
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    using TextFx;
+    using TextFx.ABNF;
+    using TextFx.ABNF.Core;
+
+    using Xunit;
+
+    public class RequiredDelimitedListTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            var sl = new StringLexerFactory(new CaseInsensitiveTerminalLexerFactory());
+            var listItemLexer = new AlternativeLexer(sl.Create("foo"), sl.Create("bar"), sl.Create("charlie"));
+            yield return new object[] { "foo,bar", "foo, bar", listItemLexer };
+
+            yield return new object[] { "foo ,bar,", "foo, bar", listItemLexer };
+
+            yield return new object[] { "foo , ,bar,charlie   ", "foo, bar, charlie", listItemLexer };
+        }
+
+        [Theory]
+        [MemberData("GetTestData")]
+        public void GetWellFormedText_NormalizesWhiteSpace(string input, string expected, ILexer listItemLexer)
+        {
+            var optionalDelimitedList = CreateTestCase(input, listItemLexer);
+            var result = optionalDelimitedList.GetWellFormedText();
+            Assert.Equal(expected, result);
+        }
+
+        private static RequiredDelimitedList CreateTestCase(string input, ILexer listItemLexer)
+        {
+            var optionLexerFactory = new OptionLexerFactory();
+            var sequenceLexerFactory = new SequenceLexerFactory();
+            var alternativeLexerFactory = new AlternativeLexerFactory();
+            var caseInsensitiveTerminalLexerFactory = new CaseInsensitiveTerminalLexerFactory();
+            var stringLexerFactory = new StringLexerFactory(caseInsensitiveTerminalLexerFactory);
+            var spaceLexerFactory = new SpaceLexerFactory(caseInsensitiveTerminalLexerFactory);
+            var repetitionLexerFactory = new RepetitionLexerFactory();
+            var horizontalTabLexerFactory = new HorizontalTabLexerFactory(caseInsensitiveTerminalLexerFactory);
+            var whiteSpaceLexerFactory = new WhiteSpaceLexerFactory(spaceLexerFactory, horizontalTabLexerFactory, alternativeLexerFactory);
+            var optionalWhiteSpaceLexerFactory = new OptionalWhiteSpaceLexerFactory(repetitionLexerFactory, whiteSpaceLexerFactory);
+            var lexerFactory = new RequiredDelmitedListLexerFactory(
+                repetitionLexerFactory,
+                sequenceLexerFactory,
+                optionLexerFactory,
+                stringLexerFactory,
+                optionalWhiteSpaceLexerFactory,
+                listItemLexer);
+
+            var encoding = Encoding.GetEncoding("us-ascii");
+            var bytes = encoding.GetBytes(input);
+            using (var ms = new MemoryStream(bytes))
+            using (var scanner = new TextScanner(ms, encoding))
+            {
+                scanner.Read();
+                return lexerFactory.Create().Read(scanner);
+            }
+        }
+    }
+}
