4b7a6e7c532a196a549b8213f713debcd460f6c3
Steven Liekens <steven.liekens@gmail.com>
2015-06-20 21:30:26 +0200

    Adding stubs for 'field-name'

---
+++ src/Http.Grammar/field-name/FieldNameLexer.cs
@@ -0,0 +1,14 @@
+﻿namespace Http.Grammar
+{
+    using System;
+
+    using TextFx;
+
+    public class FieldNameLexer : Lexer<FieldName>
+    {
+        public override bool TryRead(ITextScanner scanner, out FieldName element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Http.Grammar/field-name/FieldNameLexerFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Http.Grammar
+{
+    using System;
+
+    using TextFx;
+
+    public class FieldNameLexerFactory : ILexerFactory<FieldName>
+    {
+        public ILexer<FieldName> Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
