4176cb8db0ad410e786edb786b477227a4d4a9c2
Steven Liekens <steven.liekens@gmail.com>
2015-06-20 21:31:33 +0200

    Adding stubs for 'field-content'

---
+++ src/Http.Grammar/field-content/FieldContent.cs
@@ -0,0 +1,12 @@
+﻿namespace Http.Grammar
+{
+    using TextFx.ABNF;
+
+    public class FieldContent : Sequence
+    {
+        public FieldContent(Sequence sequence)
+            : base(sequence)
+        {
+        }
+    }
+}
---
+++ src/Http.Grammar/field-content/FieldContentLexer.cs
@@ -0,0 +1,14 @@
+namespace Http.Grammar
+{
+    using System;
+
+    using TextFx;
+
+    public class FieldContentLexer : Lexer<FieldContent>
+    {
+        public override bool TryRead(ITextScanner scanner, out FieldContent element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
