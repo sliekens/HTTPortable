fd54085d464b57cf34d6b064ac60b6cb259a1e2e
Steven Liekens <steven.liekens@gmail.com>
2015-06-20 18:37:00 +0200

    Changing the ctor of 'Pseudonym'

--- src/Http.Grammar/pseudonym/Pseudonym.cs
+++ src/Http.Grammar/pseudonym/Pseudonym.cs
@@ -1,11 +1,9 @@
 ﻿namespace Http.Grammar
 {
-    using TextFx.ABNF;
-
     public class Pseudonym : Token
     {
-        public Pseudonym(Repetition repetition)
-            : base(repetition)
+        public Pseudonym(Token token)
+            : base(token)
         {
         }
     }
