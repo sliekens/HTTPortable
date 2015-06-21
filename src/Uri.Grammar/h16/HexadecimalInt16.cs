3b0dcf3709e9461cb68172c1ea647dd8aed4d2be
Steven Liekens <steven.liekens@gmail.com>
2015-06-21 13:23:24 +0200

    Adding 'HexadecimalInt16.GetBytes()'

--- src/Uri.Grammar/h16/HexadecimalInt16.cs
+++ src/Uri.Grammar/h16/HexadecimalInt16.cs
@@ -1,5 +1,7 @@
 ﻿namespace Uri.Grammar
 {
+    using System.Globalization;
+
     using TextFx.ABNF;
 
     public class HexadecimalInt16 : Repetition
@@ -7,6 +9,21 @@
         public HexadecimalInt16(Repetition sequence)
             : base(sequence)
         {
+        }
+
+        public byte[] GetBytes()
+        {
+            var bytes = new byte[2];
+            int ix = 1;
+            var hex = this.Value;
+            for (int i = hex.Length; i > 0 && ix >= 0; i -= 2, ix--)
+            {
+                int subLength = i == 1 ? 1 : 2;
+                var substr = hex.Substring(i - subLength, subLength);
+                bytes[ix] = byte.Parse(substr, NumberStyles.HexNumber);
+            }
+
+            return bytes;
         }
     }
 }
