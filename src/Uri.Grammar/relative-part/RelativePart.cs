4399a73cfe5d4d4a7b14871134ee8b7d3a3861bf
Steven Liekens <steven.liekens@gmail.com>
2015-07-25 23:44:05 +0200

    Adding convenience properties to RelativePart

--- src/Uri.Grammar/relative-part/RelativePart.cs
+++ src/Uri.Grammar/relative-part/RelativePart.cs
@@ -8,5 +8,72 @@
             : base(alternative)
         {
         }
+
+        public Authority Authority
+        {
+            get
+            {
+                if (this.Ordinal != 1)
+                {
+                    return null;
+                }
+
+                var sequence = (Sequence)this.Element;
+                return (Authority)sequence.Elements[1];
+            }
+        }
+
+        public PathAbsolute PathAbsolute
+        {
+            get
+            {
+                if (this.Ordinal != 2)
+                {
+                    return null;
+                }
+
+                return (PathAbsolute)this.Element;
+            }
+        }
+
+        public PathAbsoluteOrEmpty PathAbsoluteOrEmpty
+        {
+            get
+            {
+                if (this.Ordinal != 1)
+                {
+                    return null;
+                }
+
+                var sequence = (Sequence)this.Element;
+                return (PathAbsoluteOrEmpty)sequence.Elements[2];
+            }
+        }
+
+        public PathEmpty PathEmpty
+        {
+            get
+            {
+                if (this.Ordinal != 4)
+                {
+                    return null;
+                }
+
+                return (PathEmpty)this.Element;
+            }
+        }
+
+        public PathNoScheme PathNoScheme
+        {
+            get
+            {
+                if (this.Ordinal != 3)
+                {
+                    return null;
+                }
+
+                return (PathNoScheme)this.Element;
+            }
+        }
     }
 }
