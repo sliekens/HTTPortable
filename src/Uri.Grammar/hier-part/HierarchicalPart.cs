f4bceef55cffa04ad7ce96203eac27da42b09cc4
Steven Liekens <steven.liekens@gmail.com>
2015-07-25 21:08:57 +0200

    Adding convenience properties to HierarchicalPart

--- src/Uri.Grammar/hier-part/HierarchicalPart.cs
+++ src/Uri.Grammar/hier-part/HierarchicalPart.cs
@@ -8,5 +8,72 @@
             : base(alternative)
         {
         }
+
+        public Authority Authority
+        {
+            get
+            {
+                if (this.Ordinal != 1)
+                {
+                    return null;
+                }
+
+                var sequence = (Sequence)this.Element;
+                return (Authority)sequence.Elements[1];
+            }
+        }
+
+        public PathAbsoluteOrEmpty PathAbsoluteOrEmpty
+        {
+            get
+            {
+                if (this.Ordinal != 1)
+                {
+                    return null;
+                }
+
+                var sequence = (Sequence)this.Element;
+                return (PathAbsoluteOrEmpty)sequence.Elements[2];
+            }
+        }
+
+        public PathAbsolute PathAbsolute
+        {
+            get
+            {
+                if (this.Ordinal != 2)
+                {
+                    return null;
+                }
+
+                return (PathAbsolute)this.Element;
+            }
+        }
+
+        public PathRootless PathRootless
+        {
+            get
+            {
+                if (this.Ordinal != 3)
+                {
+                    return null;
+                }
+
+                return (PathRootless)this.Element;
+            }
+        }
+
+        public PathEmpty PathEmpty
+        {
+            get
+            {
+                if (this.Ordinal != 4)
+                {
+                    return null;
+                }
+
+                return (PathEmpty)this.Element;
+            }
+        }
     }
 }
