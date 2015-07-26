64b8165f85545fa8fbdf6acd23b6860c85ec97d6
Steven Liekens <steven.liekens@gmail.com>
2015-07-26 14:59:08 +0200

    Adding convenience properties to Authority

--- src/Uri.Grammar/authority/Authority.cs
+++ src/Uri.Grammar/authority/Authority.cs
@@ -8,5 +8,37 @@
             : base(sequence)
         {
         }
+
+        public Host Host => (Host)this.Elements[1];
+
+        public Port Port
+        {
+            get
+            {
+                var optionalPort = (Repetition)this.Elements[2];
+                if (optionalPort.Elements.Count == 0)
+                {
+                    return null;
+                }
+
+                var portSequence = (Sequence)optionalPort.Elements[0];
+                return (Port)portSequence.Elements[1];
+            }
+        }
+
+        public UserInformation UserInformation
+        {
+            get
+            {
+                var optionalUserInfo = (Repetition)this.Elements[0];
+                if (optionalUserInfo.Elements.Count == 0)
+                {
+                    return null;
+                }
+
+                var userInfoSequence = (Sequence)optionalUserInfo.Elements[0];
+                return (UserInformation)userInfoSequence.Elements[0];
+            }
+        }
     }
 }
