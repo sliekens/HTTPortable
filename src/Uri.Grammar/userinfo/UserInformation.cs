53d91263ebb7dacf4cb40202143b87d8e700879f
Steven Liekens <steven.liekens@gmail.com>
2015-07-26 14:58:50 +0200

    Adding convenience properties to UserInformation

--- src/Uri.Grammar/userinfo/UserInformation.cs
+++ src/Uri.Grammar/userinfo/UserInformation.cs
@@ -8,5 +8,38 @@
             : base(sequence)
         {
         }
+
+        public string Password
+        {
+            get
+            {
+                var indexOfSeparator = this.Value.IndexOf(':');
+                if (indexOfSeparator == -1)
+                {
+                    return null;
+                }
+
+                return this.Value.Substring(indexOfSeparator + 1);
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                if (this.Value == string.Empty)
+                {
+                    return null;
+                }
+
+                var indexOfSeparator = this.Value.IndexOf(':');
+                if (indexOfSeparator == -1)
+                {
+                    return this.Value;
+                }
+
+                return this.Value.Substring(0, indexOfSeparator);
+            }
+        }
     }
 }
