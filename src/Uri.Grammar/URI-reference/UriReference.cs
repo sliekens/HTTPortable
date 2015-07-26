96af91708673dea8dba90a112849d2df69ec98fd
Steven Liekens <steven.liekens@gmail.com>
2015-07-26 15:05:11 +0200

    Adding convencience properties to UriReference

--- src/Uri.Grammar/URI-reference/UriReference.cs
+++ src/Uri.Grammar/URI-reference/UriReference.cs
@@ -1,5 +1,7 @@
 ﻿namespace Uri.Grammar
 {
+    using System.Diagnostics;
+
     using TextFx.ABNF;
 
     public class UriReference : Alternative
@@ -7,6 +9,46 @@
         public UriReference(Alternative alternative)
             : base(alternative)
         {
+        }
+
+        public bool IsAbsolute
+        {
+            get
+            {
+                Debug.Assert(this.Ordinal == 1 || this.Ordinal == 2, "this.Ordinal == 1 || this.Ordinal == 2");
+                if (this.Ordinal == 1)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public RelativeReference RelativeReference
+        {
+            get
+            {
+                if (this.Ordinal != 2)
+                {
+                    return null;
+                }
+
+                return (RelativeReference)this.Element;
+            }
+        }
+
+        public UniformResourceIdentifier UniformResourceIdentifier
+        {
+            get
+            {
+                if (this.Ordinal != 1)
+                {
+                    return null;
+                }
+
+                return (UniformResourceIdentifier)this.Element;
+            }
         }
     }
 }
