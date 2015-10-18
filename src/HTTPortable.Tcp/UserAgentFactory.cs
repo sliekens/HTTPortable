e81cb3ece55aab20f1bd69da8234ab4bbf455c2c
Steven Liekens <steven.liekens@gmail.com>
2015-10-18 20:21:31 +0200

    Add a basic integration test

--- src/HTTPortable.Tcp/UserAgentFactory.cs
+++ src/HTTPortable.Tcp/UserAgentFactory.cs
@@ -3,6 +3,7 @@
     using System;
     using System.IO;
     using System.Net;
+    using System.Net.Security;
     using System.Net.Sockets;
     using System.Threading;
     using System.Threading.Tasks;
@@ -16,7 +17,14 @@
             var endpoint = new DnsEndPoint(uri.DnsSafeHost, uri.Port, AddressFamily.InterNetwork);
             var ar = socket.BeginConnect(endpoint, asyncResult => socket.EndConnect(asyncResult), null);
             await Task.Run(() => ar.AsyncWaitHandle.WaitOne(), cancellationToken);
-            var stream = new NetworkStream(socket, FileAccess.ReadWrite, true);
+            Stream stream = new NetworkStream(socket, FileAccess.ReadWrite, true);
+            if (string.Equals(uri.Scheme, "https", StringComparison.OrdinalIgnoreCase))
+            {
+                var sslStream = new SslStream(stream);
+                await sslStream.AuthenticateAsClientAsync(uri.Host).ConfigureAwait(false);
+                stream = sslStream;
+            }
+
             return new UserAgent(stream);
         }
     }
