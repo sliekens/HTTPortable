646a14420bb838f46e15dc4e6a9a9bf1a15d42e1
Steven Liekens <steven.liekens@gmail.com>
2015-10-18 20:51:31 +0200

    Implementing 'token'

--- src/Http.Grammar/token/TokenLexer.cs
+++ src/Http.Grammar/token/TokenLexer.cs
@@ -3,12 +3,46 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class TokenLexer : Lexer<Token>
     {
+        private readonly ILexer<Repetition> innerLexer;
+
+        public TokenLexer(ILexer<Repetition> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<Token> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return
+                    ReadResult<Token>.FromError(
+                        new SyntaxError
+                            {
+                                Message = "Expected 'token'.",
+                                RuleName = "token",
+                                Context = scanner.GetContext(),
+                                InnerError = result.Error
+                            });
+            }
+
+            var element = new Token(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<Token>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/token/TokenLexerFactory.cs
+++ src/Http.Grammar/token/TokenLexerFactory.cs
@@ -3,12 +3,37 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class TokenLexerFactory : ILexerFactory<Token>
     {
+        private readonly IRepetitionLexerFactory repetitionLexerFactory;
+
+        private readonly ILexerFactory<TokenCharacter> tokenCharacterLexerFactory;
+
+        public TokenLexerFactory(
+            IRepetitionLexerFactory repetitionLexerFactory,
+            ILexerFactory<TokenCharacter> tokenCharacterLexerFactory)
+        {
+            if (repetitionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(repetitionLexerFactory));
+            }
+            if (tokenCharacterLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(tokenCharacterLexerFactory));
+            }
+            this.repetitionLexerFactory = repetitionLexerFactory;
+            this.tokenCharacterLexerFactory = tokenCharacterLexerFactory;
+        }
+
         public ILexer<Token> Create()
         {
-            throw new NotImplementedException();
+            var innerLexer = this.repetitionLexerFactory.Create(
+                this.tokenCharacterLexerFactory.Create(),
+                1,
+                int.MaxValue);
+            return new TokenLexer(innerLexer);
         }
     }
 }
