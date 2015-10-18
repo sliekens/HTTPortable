634b748c41d697bae70dabe26759fd53096aa2b3
Steven Liekens <steven.liekens@gmail.com>
2015-10-18 21:33:28 +0200

    Implement 'pseudonym'

--- src/Http.Grammar/pseudonym/PseudonymLexer.cs
+++ src/Http.Grammar/pseudonym/PseudonymLexer.cs
@@ -6,9 +6,42 @@
 
     public class PseudonymLexer : Lexer<Pseudonym>
     {
+        private readonly ILexer<Token> innerLexer;
+
+        public PseudonymLexer(ILexer<Token> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<Pseudonym> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return
+                    ReadResult<Pseudonym>.FromError(
+                        new SyntaxError
+                        {
+                            Message = "Expected 'pseudonym'.",
+                            RuleName = "pseudonym",
+                            Context = scanner.GetContext(),
+                            InnerError = result.Error
+                        });
+            }
+
+            var element = new Pseudonym(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<Pseudonym>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/pseudonym/PseudonymLexerFactory.cs
+++ src/Http.Grammar/pseudonym/PseudonymLexerFactory.cs
@@ -6,9 +6,22 @@
 
     public class PseudonymLexerFactory : ILexerFactory<Pseudonym>
     {
+        private readonly ILexerFactory<Token> tokenLexerFactory;
+
+        public PseudonymLexerFactory(ILexerFactory<Token> tokenLexerFactory)
+        {
+            if (tokenLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(tokenLexerFactory));
+            }
+
+            this.tokenLexerFactory = tokenLexerFactory;
+        }
+
         public ILexer<Pseudonym> Create()
         {
-            throw new NotImplementedException();
+            var innerLexer = this.tokenLexerFactory.Create();
+            return new PseudonymLexer(innerLexer);
         }
     }
 }
