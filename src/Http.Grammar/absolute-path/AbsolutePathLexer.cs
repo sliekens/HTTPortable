b67d4ab69b943ce83e0a1275b98c145e702c272e
Steven Liekens <steven.liekens@gmail.com>
2015-10-18 21:45:13 +0200

    Implement 'absolute-path'

--- src/Http.Grammar/absolute-path/AbsolutePathLexer.cs
+++ src/Http.Grammar/absolute-path/AbsolutePathLexer.cs
@@ -3,12 +3,46 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class AbsolutePathLexer : Lexer<AbsolutePath>
     {
+        private readonly ILexer<Repetition> innerLexer;
+
+        public AbsolutePathLexer(ILexer<Repetition> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<AbsolutePath> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return
+                    ReadResult<AbsolutePath>.FromError(
+                        new SyntaxError
+                        {
+                            Message = "Expected 'absolute-path'.",
+                            RuleName = "absolute-path",
+                            Context = scanner.GetContext(),
+                            InnerError = result.Error
+                        });
+            }
+
+            var element = new AbsolutePath(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<AbsolutePath>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/absolute-path/AbsolutePathLexerFactory.cs
+++ src/Http.Grammar/absolute-path/AbsolutePathLexerFactory.cs
@@ -3,12 +3,59 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
+
+    using Uri.Grammar;
 
     public class AbsolutePathLexerFactory : ILexerFactory<AbsolutePath>
     {
+        private readonly IRepetitionLexerFactory repetitionLexerFactory;
+
+        private readonly ILexerFactory<Segment> segmentLexerFactory;
+
+        private readonly ISequenceLexerFactory sequenceLexerFactory;
+
+        private readonly ITerminalLexerFactory terminalLexerFactory;
+
+        public AbsolutePathLexerFactory(
+            IRepetitionLexerFactory repetitionLexerFactory,
+            ISequenceLexerFactory sequenceLexerFactory,
+            ITerminalLexerFactory terminalLexerFactory,
+            ILexerFactory<Segment> segmentLexerFactory)
+        {
+            if (repetitionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(repetitionLexerFactory));
+            }
+
+            if (sequenceLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(sequenceLexerFactory));
+            }
+
+            if (terminalLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(terminalLexerFactory));
+            }
+
+            if (segmentLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(segmentLexerFactory));
+            }
+
+            this.repetitionLexerFactory = repetitionLexerFactory;
+            this.sequenceLexerFactory = sequenceLexerFactory;
+            this.terminalLexerFactory = terminalLexerFactory;
+            this.segmentLexerFactory = segmentLexerFactory;
+        }
+
         public ILexer<AbsolutePath> Create()
         {
-            throw new NotImplementedException();
+            var a = this.terminalLexerFactory.Create(@"/", StringComparer.Ordinal);
+            var b = this.segmentLexerFactory.Create();
+            var c = this.sequenceLexerFactory.Create(a, b);
+            var innerLexer = this.repetitionLexerFactory.Create(c, 1, int.MaxValue);
+            return new AbsolutePathLexer(innerLexer);
         }
     }
 }
