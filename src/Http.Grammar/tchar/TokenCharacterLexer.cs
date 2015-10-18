793e5a053c48a506674c42e1cdf344c968a840bb
Steven Liekens <steven.liekens@gmail.com>
2015-10-18 20:35:41 +0200

    Implement 'tchar'

--- src/Http.Grammar/tchar/TokenCharacterLexer.cs
+++ src/Http.Grammar/tchar/TokenCharacterLexer.cs
@@ -3,12 +3,44 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class TokenCharacterLexer : Lexer<TokenCharacter>
     {
+        private readonly ILexer<Alternative> innerLexer;
+
+        public TokenCharacterLexer(ILexer<Alternative> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<TokenCharacter> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return ReadResult<TokenCharacter>.FromError(new SyntaxError
+                {
+                    Message = "Expected 'tchar'.",
+                    RuleName = "tchar",
+                    Context = scanner.GetContext(),
+                    InnerError = result.Error
+                });
+            }
+
+            var element = new TokenCharacter(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<TokenCharacter>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/tchar/TokenCharacterLexerFactory.cs
+++ src/Http.Grammar/tchar/TokenCharacterLexerFactory.cs
@@ -3,12 +3,73 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
+    using TextFx.ABNF.Core;
 
     public class TokenCharacterLexerFactory : ILexerFactory<TokenCharacter>
     {
+        private readonly ILexerFactory<Alpha> alphaLexerFactory;
+
+        private readonly IAlternativeLexerFactory alternativeLexerFactory;
+
+        private readonly ILexerFactory<Digit> digitLexerFactory;
+
+        private readonly ITerminalLexerFactory terminalLexerFactory;
+
+        public TokenCharacterLexerFactory(
+            ITerminalLexerFactory terminalLexerFactory,
+            ILexerFactory<Alpha> alphaLexerFactory,
+            ILexerFactory<Digit> digitLexerFactory,
+            IAlternativeLexerFactory alternativeLexerFactory)
+        {
+            if (terminalLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(terminalLexerFactory));
+            }
+
+            if (alphaLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(alphaLexerFactory));
+            }
+
+            if (digitLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(digitLexerFactory));
+            }
+
+            if (alternativeLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(alternativeLexerFactory));
+            }
+
+            this.terminalLexerFactory = terminalLexerFactory;
+            this.alphaLexerFactory = alphaLexerFactory;
+            this.digitLexerFactory = digitLexerFactory;
+            this.alternativeLexerFactory = alternativeLexerFactory;
+        }
+
         public ILexer<TokenCharacter> Create()
         {
-            throw new NotImplementedException();
+            var innerLexer =
+                this.alternativeLexerFactory.Create(
+                    this.terminalLexerFactory.Create(@"!", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"#", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"$", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"%", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"&", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"'", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"*", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"+", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"-", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@".", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"^", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"_", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"`", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"|", StringComparer.Ordinal),
+                    this.terminalLexerFactory.Create(@"~", StringComparer.Ordinal),
+                    this.digitLexerFactory.Create(),
+                    this.alphaLexerFactory.Create());
+            return new TokenCharacterLexer(innerLexer);
         }
     }
 }
