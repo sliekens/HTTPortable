73e0fdba000ce1caf65813ba113d62a2db2b975e
Steven Liekens <steven.liekens@gmail.com>
2015-10-20 19:34:02 +0200

    Implement 'chunk-data'

--- src/Http.Grammar/chunk-data/ChunkDataLexer.cs
+++ src/Http.Grammar/chunk-data/ChunkDataLexer.cs
@@ -3,12 +3,44 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class ChunkDataLexer : Lexer<ChunkData>
     {
+        private readonly ILexer<Repetition> innerLexer;
+
+        public ChunkDataLexer(ILexer<Repetition> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<ChunkData> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return ReadResult<ChunkData>.FromError(new SyntaxError
+                {
+                    Message = "Expected 'chunk-data'.",
+                    RuleName = "chunk-data",
+                    Context = scanner.GetContext(),
+                    InnerError = result.Error
+                });
+            }
+
+            var element = new ChunkData(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<ChunkData>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/chunk-data/ChunkDataLexerFactory.cs
+++ src/Http.Grammar/chunk-data/ChunkDataLexerFactory.cs
@@ -3,12 +3,37 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
+    using TextFx.ABNF.Core;
 
     public class ChunkDataLexerFactory : ILexerFactory<ChunkData>
     {
+        private readonly ILexerFactory<Octet> octetLexerFactory;
+
+        private readonly IRepetitionLexerFactory repetitionLexerFactory;
+
+        public ChunkDataLexerFactory(
+            IRepetitionLexerFactory repetitionLexerFactory,
+            ILexerFactory<Octet> octetLexerFactory)
+        {
+            if (repetitionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(repetitionLexerFactory));
+            }
+
+            if (octetLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(octetLexerFactory));
+            }
+
+            this.repetitionLexerFactory = repetitionLexerFactory;
+            this.octetLexerFactory = octetLexerFactory;
+        }
+
         public ILexer<ChunkData> Create()
         {
-            throw new NotImplementedException();
+            var innerLexer = this.repetitionLexerFactory.Create(this.octetLexerFactory.Create(), 1, int.MaxValue);
+            return new ChunkDataLexer(innerLexer);
         }
     }
 }
