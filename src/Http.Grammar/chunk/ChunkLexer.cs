15c31f2410b704555a30aa39558a640e40fe8213
Steven Liekens <steven.liekens@gmail.com>
2015-10-20 19:21:59 +0200

    Implement 'chunk'

--- src/Http.Grammar/chunk/ChunkLexer.cs
+++ src/Http.Grammar/chunk/ChunkLexer.cs
@@ -3,12 +3,44 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class ChunkLexer : Lexer<Chunk>
     {
+        private ILexer<Sequence> innerLexer;
+
+        public ChunkLexer(ILexer<Sequence> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<Chunk> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return ReadResult<Chunk>.FromError(new SyntaxError
+                {
+                    Message = "Expected 'chunk'.",
+                    RuleName = "chunk",
+                    Context = scanner.GetContext(),
+                    InnerError = result.Error
+                });
+            }
+
+            var element = new Chunk(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<Chunk>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/chunk/ChunkLexerFactory.cs
+++ src/Http.Grammar/chunk/ChunkLexerFactory.cs
@@ -3,12 +3,78 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
+    using TextFx.ABNF.Core;
 
     public class ChunkLexerFactory : ILexerFactory<Chunk>
     {
+        private readonly ILexerFactory<ChunkData> chunkDataLexerFactory;
+
+        private readonly ILexerFactory<ChunkExtension> chunkExtensionLexerFactory;
+
+        private readonly ILexerFactory<ChunkSize> chunkSizeLexerFactory;
+
+        private readonly ILexerFactory<EndOfLine> endOfLineLexerFactory;
+
+        private readonly IOptionLexerFactory optionLexerFactory;
+
+        private readonly ISequenceLexerFactory sequenceLexerFactory;
+
+        public ChunkLexerFactory(
+            ISequenceLexerFactory sequenceLexerFactory,
+            ILexerFactory<ChunkSize> chunkSizeLexerFactory,
+            IOptionLexerFactory optionLexerFactory,
+            ILexerFactory<ChunkExtension> chunkExtensionLexerFactory,
+            ILexerFactory<EndOfLine> endOfLineLexerFactory,
+            ILexerFactory<ChunkData> chunkDataLexerFactory)
+        {
+            if (sequenceLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(sequenceLexerFactory));
+            }
+
+            if (chunkSizeLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(chunkSizeLexerFactory));
+            }
+
+            if (optionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(optionLexerFactory));
+            }
+
+            if (chunkExtensionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(chunkExtensionLexerFactory));
+            }
+
+            if (endOfLineLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(endOfLineLexerFactory));
+            }
+
+            if (chunkDataLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(chunkDataLexerFactory));
+            }
+
+            this.sequenceLexerFactory = sequenceLexerFactory;
+            this.chunkSizeLexerFactory = chunkSizeLexerFactory;
+            this.optionLexerFactory = optionLexerFactory;
+            this.chunkExtensionLexerFactory = chunkExtensionLexerFactory;
+            this.endOfLineLexerFactory = endOfLineLexerFactory;
+            this.chunkDataLexerFactory = chunkDataLexerFactory;
+        }
+
         public ILexer<Chunk> Create()
         {
-            throw new NotImplementedException();
+            var a = this.chunkSizeLexerFactory.Create();
+            var b = this.chunkExtensionLexerFactory.Create();
+            var c = this.optionLexerFactory.Create(b);
+            var d = this.endOfLineLexerFactory.Create();
+            var e = this.chunkDataLexerFactory.Create();
+            var innerLexer = this.sequenceLexerFactory.Create(a, c, d, e, d);
+            return new ChunkLexer(innerLexer);
         }
     }
 }
