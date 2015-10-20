938a51ce03cbddfad07c10583c863eafd2869977
Steven Liekens <steven.liekens@gmail.com>
2015-10-20 19:39:19 +0200

    Implement 'chunk-ext'

--- src/Http.Grammar/chunk-ext/ChunkExtensionLexer.cs
+++ src/Http.Grammar/chunk-ext/ChunkExtensionLexer.cs
@@ -3,12 +3,46 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class ChunkExtensionLexer : Lexer<ChunkExtension>
     {
+        private readonly ILexer<Repetition> innerLexer;
+
+        public ChunkExtensionLexer(ILexer<Repetition> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<ChunkExtension> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return
+                    ReadResult<ChunkExtension>.FromError(
+                        new SyntaxError
+                        {
+                            Message = "Expected 'chunk-ext'.",
+                            RuleName = "chunk-ext",
+                            Context = scanner.GetContext(),
+                            InnerError = result.Error
+                        });
+            }
+
+            var element = new ChunkExtension(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<ChunkExtension>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/chunk-ext/ChunkExtensionLexerFactory.cs
+++ src/Http.Grammar/chunk-ext/ChunkExtensionLexerFactory.cs
@@ -3,12 +3,79 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class ChunkExtensionLexerFactory : ILexerFactory<ChunkExtension>
     {
+        private readonly ILexerFactory<ChunkExtensionName> chunkExtensionNameLexerFactory;
+
+        private readonly ILexerFactory<ChunkExtensionValue> chunkExtensionValueLexerFactory;
+
+        private readonly IOptionLexerFactory optionLexerFactory;
+
+        private readonly IRepetitionLexerFactory repetitionLexerFactory;
+
+        private readonly ISequenceLexerFactory sequenceLexerFactory;
+
+        private readonly ITerminalLexerFactory terminalLexerFactory;
+
+        public ChunkExtensionLexerFactory(
+            IRepetitionLexerFactory repetitionLexerFactory,
+            ISequenceLexerFactory sequenceLexerFactory,
+            ITerminalLexerFactory terminalLexerFactory,
+            ILexerFactory<ChunkExtensionName> chunkExtensionNameLexerFactory,
+            IOptionLexerFactory optionLexerFactory,
+            ILexerFactory<ChunkExtensionValue> chunkExtensionValueLexerFactory)
+        {
+            if (repetitionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(repetitionLexerFactory));
+            }
+
+            if (sequenceLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(sequenceLexerFactory));
+            }
+
+            if (terminalLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(terminalLexerFactory));
+            }
+
+            if (chunkExtensionNameLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(chunkExtensionNameLexerFactory));
+            }
+
+            if (optionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(optionLexerFactory));
+            }
+
+            if (chunkExtensionValueLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(chunkExtensionValueLexerFactory));
+            }
+
+            this.repetitionLexerFactory = repetitionLexerFactory;
+            this.sequenceLexerFactory = sequenceLexerFactory;
+            this.terminalLexerFactory = terminalLexerFactory;
+            this.chunkExtensionNameLexerFactory = chunkExtensionNameLexerFactory;
+            this.optionLexerFactory = optionLexerFactory;
+            this.chunkExtensionValueLexerFactory = chunkExtensionValueLexerFactory;
+        }
+
         public ILexer<ChunkExtension> Create()
         {
-            throw new NotImplementedException();
+            var a = this.terminalLexerFactory.Create(@";", StringComparer.Ordinal);
+            var b = this.chunkExtensionNameLexerFactory.Create();
+            var c = this.terminalLexerFactory.Create(@"=", StringComparer.Ordinal);
+            var d = this.chunkExtensionValueLexerFactory.Create();
+            var e = this.sequenceLexerFactory.Create(c, d);
+            var f = this.optionLexerFactory.Create(e);
+            var g = this.sequenceLexerFactory.Create(a, b, e);
+            var innerLexer = this.repetitionLexerFactory.Create(g, 0, int.MaxValue);
+            return new ChunkExtensionLexer(innerLexer);
         }
     }
 }
