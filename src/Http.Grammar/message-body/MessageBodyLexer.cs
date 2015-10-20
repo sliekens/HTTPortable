ba9d4ea8444d9d0d6f025e6f5a0064bb27341761
Steven Liekens <steven.liekens@gmail.com>
2015-10-20 22:45:12 +0200

    Implement 'message-body'

--- src/Http.Grammar/message-body/MessageBodyLexer.cs
+++ src/Http.Grammar/message-body/MessageBodyLexer.cs
@@ -3,12 +3,46 @@ namespace Http.Grammar
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class MessageBodyLexer : Lexer<MessageBody>
     {
+        private readonly ILexer<Repetition> innerLexer;
+
+        public MessageBodyLexer(ILexer<Repetition> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<MessageBody> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return
+                    ReadResult<MessageBody>.FromError(
+                        new SyntaxError
+                            {
+                                Message = "Expected 'message-body'.",
+                                RuleName = "message-body",
+                                Context = scanner.GetContext(),
+                                InnerError = result.Error
+                            });
+            }
+
+            var element = new MessageBody(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<MessageBody>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/message-body/MessageBodyLexerFactory.cs
+++ src/Http.Grammar/message-body/MessageBodyLexerFactory.cs
@@ -3,12 +3,37 @@ namespace Http.Grammar
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
+    using TextFx.ABNF.Core;
 
     public class MessageBodyLexerFactory : ILexerFactory<MessageBody>
     {
+        private readonly ILexerFactory<Octet> octetLexerFactory;
+
+        private readonly IRepetitionLexerFactory repetitionLexerFactory;
+
+        public MessageBodyLexerFactory(
+            IRepetitionLexerFactory repetitionLexerFactory,
+            ILexerFactory<Octet> octetLexerFactory)
+        {
+            if (repetitionLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(repetitionLexerFactory));
+            }
+
+            if (octetLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(octetLexerFactory));
+            }
+
+            this.repetitionLexerFactory = repetitionLexerFactory;
+            this.octetLexerFactory = octetLexerFactory;
+        }
+
         public ILexer<MessageBody> Create()
         {
-            throw new NotImplementedException();
+            var innerLexer = this.repetitionLexerFactory.Create(this.octetLexerFactory.Create(), 1, int.MaxValue);
+            return new MessageBodyLexer(innerLexer);
         }
     }
 }
