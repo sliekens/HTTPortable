0dcc916ebc5e848911f16938ed60460fea3d357d
Steven Liekens <steven.liekens@gmail.com>
2015-10-20 20:52:16 +0200

    Implement 'chunk-ext-val'

--- src/Http.Grammar/chunk-ext-val/ChunkExtensionValueLexer.cs
+++ src/Http.Grammar/chunk-ext-val/ChunkExtensionValueLexer.cs
@@ -3,12 +3,46 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class ChunkExtensionValueLexer : Lexer<ChunkExtensionValue>
     {
+        private readonly ILexer<Alternative> innerLexer;
+
+        public ChunkExtensionValueLexer(ILexer<Alternative> innerLexer)
+        {
+            if (innerLexer == null)
+            {
+                throw new ArgumentNullException(nameof(innerLexer));
+            }
+
+            this.innerLexer = innerLexer;
+        }
+
         public override ReadResult<ChunkExtensionValue> Read(ITextScanner scanner, Element previousElementOrNull)
         {
-            throw new NotImplementedException();
+            var result = this.innerLexer.Read(scanner, null);
+            if (!result.Success)
+            {
+                return
+                    ReadResult<ChunkExtensionValue>.FromError(
+                        new SyntaxError
+                        {
+                            Message = "Expected 'chunk-ext-val'.",
+                            RuleName = "chunk-ext-val",
+                            Context = scanner.GetContext(),
+                            InnerError = result.Error
+                        });
+            }
+
+            var element = new ChunkExtensionValue(result.Element);
+            if (previousElementOrNull != null)
+            {
+                previousElementOrNull.NextElement = element;
+                element.PreviousElement = previousElementOrNull;
+            }
+
+            return ReadResult<ChunkExtensionValue>.FromResult(element);
         }
     }
 }
--- src/Http.Grammar/chunk-ext-val/ChunkExtensionValueLexerFactory.cs
+++ src/Http.Grammar/chunk-ext-val/ChunkExtensionValueLexerFactory.cs
@@ -3,12 +3,47 @@
     using System;
 
     using TextFx;
+    using TextFx.ABNF;
 
     public class ChunkExtensionValueLexerFactory : ILexerFactory<ChunkExtensionValue>
     {
+        private readonly IAlternativeLexerFactory alternativeLexerFactory;
+
+        private readonly ILexerFactory<QuotedString> quotedStringLexerFactory;
+
+        private readonly ILexerFactory<Token> tokenLexerFactory;
+
+        public ChunkExtensionValueLexerFactory(
+            IAlternativeLexerFactory alternativeLexerFactory,
+            ILexerFactory<Token> tokenLexerFactory,
+            ILexerFactory<QuotedString> quotedStringLexerFactory)
+        {
+            if (alternativeLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(alternativeLexerFactory));
+            }
+
+            if (tokenLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(tokenLexerFactory));
+            }
+
+            if (quotedStringLexerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(quotedStringLexerFactory));
+            }
+
+            this.alternativeLexerFactory = alternativeLexerFactory;
+            this.tokenLexerFactory = tokenLexerFactory;
+            this.quotedStringLexerFactory = quotedStringLexerFactory;
+        }
+
         public ILexer<ChunkExtensionValue> Create()
         {
-            throw new NotImplementedException();
+            var innerLexer = this.alternativeLexerFactory.Create(
+                this.tokenLexerFactory.Create(),
+                this.quotedStringLexerFactory.Create());
+            return new ChunkExtensionValueLexer(innerLexer);
         }
     }
 }
