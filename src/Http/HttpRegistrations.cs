070a0329c0d76b3264f40eef0d58aec646aec3cc
Steven Liekens <steven.liekens@gmail.com>
2016-04-28 20:00:08 +0200

    Add HttpRegistrations

---
+++ src/Http/HttpRegistrations.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Txt;
+
+namespace Http
+{
+    public class HttpRegistrations : Registrations
+    {
+        public static IEnumerable<Registration> GetRegistrations(GetInstanceDelegate getInstance)
+        {
+            return GetRegistrations(typeof(HttpRegistrations).GetTypeInfo().Assembly, getInstance);
+        }
+    }
+}
---
+++ test/Http.Tests/HttpRegistrationsTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using SimpleInjector;
+using Txt.ABNF;
+using Uri;
+using Xunit;
+using Registration = Txt.Registration;
+
+namespace Http
+{
+    public class HttpRegistrationsTest
+    {
+        private readonly Container container = new Container();
+
+        [Fact]
+        public void VerifyRegistrations()
+        {
+            List<Registration> registrations = new List<Registration>();
+            registrations.AddRange(AbnfRegistrations.GetRegistrations(container.GetInstance));
+            registrations.AddRange(UriRegistrations.GetRegistrations(container.GetInstance));
+            registrations.AddRange(HttpRegistrations.GetRegistrations(container.GetInstance));
+            foreach (var registration in registrations)
+            {
+                if (registration.Implementation != null)
+                {
+                    container.RegisterSingleton(registration.Service, registration.Implementation);
+                }
+                if (registration.Instance != null)
+                {
+                    container.RegisterSingleton(registration.Service, registration.Instance);
+                }
+                if (registration.Factory != null)
+                {
+                    container.RegisterSingleton(registration.Service, registration.Factory);
+                }
+            }
+            container.Verify();
+        }
+    }
+}
